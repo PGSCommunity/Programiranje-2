7d1288e5f8aec3f945956d506f9bede13b396934
Danin Sadzak <daninsadzak@gmail.com>
2023-04-06 00:21:03 +0200

    Vjezba - Sortiranje - Postavljen zadatak 1

---
+++ Pripreme/Sortiranje/Zadatak_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+1.	Za učitanu dimenziju niza i cijeli broj m, Koristeci funkcije ispisati članove niza sortirane u rastućem poretku,
+a zatim vrijednost kuba broja prvog manjeg od učitanog broja m.
+*/
