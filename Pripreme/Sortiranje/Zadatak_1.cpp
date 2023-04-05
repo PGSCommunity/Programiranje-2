a60a3cfbe2500e44886ab50e328bd49646e3939b
Danin Sadzak <daninsadzak@gmail.com>
2023-04-06 00:48:10 +0200

    Pripreme - Sortiranje - Zavrsen Zadatak 1

--- Pripreme/Sortiranje/Zadatak_1.cpp
+++ Pripreme/Sortiranje/Zadatak_1.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void sort(int m) {
+    int niz[m];
+    for(int i = 0; i < m; i++) {
+        cin >> niz[i];
+    }
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < m - 1 - i; j++) {
+            if(niz[j] > niz[j + 1]) {
+                int temp = niz[j + 1];
+                niz[j + 1] = niz[j];
+                niz[j] = temp;
+            }
+        }
+    }
+    int tmin(m);
+    cout << "Sortiran niz:\n";
+    for(int i = 0; i < m; i++) {
+        cout << niz[i] << " ";
+        if(tmin > niz[i]) tmin = niz[i];
+    }
+    cout << '\n';
+    cout << "Najmanji broj: " << tmin;
+}
 
+int main() {
+    int m;
+    cin >> m;
+    sort(m);
     return 0;
 }
 /*
