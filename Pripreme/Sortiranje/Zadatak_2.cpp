c9785bc9805a8a4e867959fe2e88b1d63df501c6
Danin Sadzak <daninsadzak@gmail.com>
2023-04-06 00:51:25 +0200

    Pripreme - Sortiranje - Postavljen zadatak 2

---
+++ Pripreme/Sortiranje/Zadatak_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+2.	Učitavati realne brojeve sve dok ne unesemo broj 0 ili dok ne popunimo niz od 10 brojeva. 
+Ispisati sve brojeve manje od aritmetičke sredine u rastućem poretku. 
+*/
