98cd948a4e4015d1d5b797807f95978ea7147763
Danin Sadzak <daninsadzak@gmail.com>
2023-04-06 18:05:53 +0200

    Priprema - Sortiranje - Postavljen 6. zadatak

---
+++ Pripreme/Sortiranje/Zadatak_6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+6.	Kreirati strukturu podataka Ucenik( ime_prezime, razred, prosjek). Sortirati podatke prema svakom od navedenih 
+članova strukture. Ispisati sve učenike 2. razreda, prosjecnu ocjenu svih ucenika prvih razreda i imena i prezimena
+ucenika svih razreda sa prosječnom ocjenom 5.0. (koristiti funkcije).
+*/
