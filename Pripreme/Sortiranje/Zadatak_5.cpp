74cf3110bf1e685f5f887fe0a90dcf76caf2de4d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-06 17:36:15 +0200

    Pripreme - Sortiranje - Postavljen 5. zadatak

---
+++ Pripreme/Sortiranje/Zadatak_5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+5.	Kreirati strukturu podataka SportskiKlub ( br_igraca, prezime, zarada). Sortirati podatke prema svakom od 
+navedenih člana strukture. Ispisati sve igrače kluba čija je plata veća od 1 000 000$. (koristiti funkcije)
+*/
