8c1ba68f98339f9508129dd2b150d8267629cf0a
Danin Sadzak <daninsadzak@gmail.com>
2023-04-06 18:02:54 +0200

    Pripreme - Sortiranje - Zavrsen 5. zadatak + dodan settings

--- Pripreme/Sortiranje/Zadatak_5.cpp
+++ Pripreme/Sortiranje/Zadatak_5.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
+struct SportskiKlub {
+    int br_igraca;
+    string prezime;
+    float zarada;
+};
 
+int main() {
+    SportskiKlub igraci;
+    cout << "Broj igraca?\n";
+    cin >> igraci.br_igraca;
+
+    SportskiKlub sp[igraci.br_igraca];
+    for(int i = 0; i < igraci.br_igraca; i++) {
+        cout << "Unesite prezime " << i + 1 << ". igraca!\n";
+        cin >> sp[i].prezime;
+        cout << "Unesite zaradu " << i + 1 << ". igraca!\n";
+        cin >> sp[i].zarada;
+    }
+
+    for(int i = 0; i < igraci.br_igraca; i++) {
+        for(int j = 0; j < igraci.br_igraca - 1 - i; j++) {
+            if(sp[j].prezime > sp[j + 1].prezime) {
+                string temp = sp[j + 1].prezime;
+                sp[j + 1].prezime = sp[j].prezime;
+                sp[j].prezime = temp;
+            }
+        }
+    }
+    for(int i = 0; i < igraci.br_igraca; i++) {
+        for(int j = 0; j < igraci.br_igraca - 1 - i; j++) {
+            if(sp[j].zarada > sp[j + 1].zarada) {
+                float temp = sp[j + 1].zarada;
+                sp[j + 1].zarada = sp[j].zarada;
+                sp[j].zarada = temp;
+            }
+        }
+    }
+
+    cout << "Prezimena sortirana:\n";
+    for(int i = 0; i < igraci.br_igraca; i++) {
+        cout << sp[i].prezime << " ";
+    }
+    cout << '\n';
+    cout << "Zarada sortirana:\n";
+    for(int i = 0; i < igraci.br_igraca; i++) {
+        cout << sp[i].zarada << " ";
+    }
+
+    cout << '\n';
+    cout << "Igraci cija je zarada veca od milion:\n";
+    for(int i = 0; i < igraci.br_igraca; i++) {
+        if(sp[i].zarada > 1000000) cout << sp[i].prezime << '\n';
+    }
     return 0;
 }
 /*
