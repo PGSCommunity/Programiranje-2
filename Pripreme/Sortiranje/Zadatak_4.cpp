94a140a2314ee634d59127378152ba9b2f6b8992
Danin Sadzak <daninsadzak@gmail.com>
2023-04-06 17:12:05 +0200

    Pripreme - Sortiranje - Postavljen 4. zadatak

---
+++ Pripreme/Sortiranje/Zadatak_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+4.	Napisati program koji omogućava unos članova niza dimenzije k, i broja b. Potom se formira broj n koji predstavlja
+ sumu brojeva niza manjih od broja b (broj b nije uključen), a zatim se formira funkcija najveci_stepen koja prihvata 
+ prirodan broj n, a vraća najveći stepen broja 2 koji je manji ili jednak broju n. 
+ Primjer, za n=35 funkcija treba vratiti 5, jer je 32 najveći broj koji je stepen broja 2 (25=32), a koji je manji ili
+ jednak n, tj. od 35. Nije dozvoljeno koristiti funkcije iz biblioteke cmath.
+*/
