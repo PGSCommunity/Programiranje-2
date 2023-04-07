0200adfc4dfd43f95eba14978737187370ae7af9
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 00:31:16 +0200

    Pripreme - Binarna Pretraga - Postavljen 2. zadatak

---
+++ Pripreme/Binarna Pretraga/Zadatak_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+2.	Učitati N brojeva u niz, i broj M. Izračunati geometrijsku sredinu do broja M i harmonijsku sredinu brojeva od 
+broja M.
+*/
