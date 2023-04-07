395ba2c19b3c131112e5681ddaf8b3247d2ea416
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 01:53:22 +0200

    Vjezbe - Binarna Pretraga - Postavljen 1. zadatak (Drugi nacin)

---
+++ Vjezbe/Binarna Pretraga/Zadatak_1_Drugi_Nacin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji sa standardnog ulaza unosi leksikografski uređen niz od maksimalno 20 znakova i provjerava da li
+niz sadrži znak @. Zadatak uraditi na dva načina - koristeći linearnu pa binarnu pretragu.
+*/
