a32f5f5aad724df8433b8b948a07e8920f1dd816
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 00:58:41 +0200

    Priprema - Binarna Pretraga - Zavrsen 3. zadatak

--- Pripreme/Binarna Pretraga/Zadatak_3.cpp
+++ Pripreme/Binarna Pretraga/Zadatak_3.cpp
@@ -2,7 +2,41 @@
 using namespace std;
 
 int main() {
+    int x, a, b;
+    cout << "Velicina niza?\n";
+    cin >> x;
+    int niz[x];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < x; i++) {
+        cin >> niz[i];
+    }
 
+    cout << "Unesite brojeve a i b!\n";
+    unos:cin >> a >> b;
+    if(b < a) {
+        cout << "Necatan upis! Treba biti (a < b)!\n";
+        goto unos;
+    }
+
+    float suma(0), br(0);
+    int l(0), d = x - 1;
+    while(l <= d) {
+        int s = (l + d) / 2;
+        if(niz[s] > a) {
+            suma += niz[s];
+            br++;
+            l = s + 1;
+        }
+        else if(niz[s] < b) {
+            suma += niz[s];
+            br++;
+            d = s - 1;
+        }
+        else {
+            l = s + 1;
+        }
+    }
+    cout << "Aritmeticka sredina izmedju brojeva a i b: " << suma / br;
     return 0;
 }
 /*
