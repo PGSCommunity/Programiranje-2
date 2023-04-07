586f79fba89b09a84fe0c03e386464f2336b0896
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 00:48:02 +0200

    Pripreme - Binarna Pretraga - Postavljen 1. zadatak

---
+++ Pripreme/Binarna Pretraga/Zadatak_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+3.	Učitati niz od X brojeva i dva prirodna broja a i b (a < b),  pa izračunati aritmetičku sredinu brojeva izmedju 
+pozicija određenih brojevima a i b.
+*/
