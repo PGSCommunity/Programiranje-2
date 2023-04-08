1fec010fa2527b985b18f8c290ea2ff15836a87d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 21:10:07 +0200

    Vjezbe - Sortiranje - Postavljen 12. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+U niz ucitavati brojeve dok se ne unese 0 ili dok ne popunimo niz od 8 brojeva.Ispisati sve brojeve vece od 
+aritmeticke sredine u rastucem poretku.
+*/
