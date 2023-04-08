ef375f4dc93f6c650b459c0430210e2f4a5f4234
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 03:16:24 +0200

    Vjezbe - Binarna Pretraga - Postavljen 3. zadatak

---
+++ Vjezbe/Binarna Pretraga/Zadatak_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji pronalazi element X u učitanom nizu dimenzije k – elementi niza su u neuređenom poretku.
+Potom se računa suma elemenata većih od broja a.(binarna pretraga)
+*/
