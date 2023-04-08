17f638b4ae4b05a0da94e69f7b0d699bc7b357da
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 15:03:52 +0200

    Vjezbe - Sortiranje - Postavljen 2. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji omogućava unos elemenata niza dimenzije 7. Članove niza je potrebno sortirati u obrnutom poretku
+(od većeg elementa ka manjem). Program treba da sadrži 3 funkcije (unos, sortiranje, ispis).
+*/
