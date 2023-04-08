3f8b5e2cc15e6c8826a8765a81f1bcd741a7a092
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 02:13:41 +0200

    Vjezbe - Binarna Pretraga - Zavrsen 1. zadatak (Drugi nacin)

--- Vjezbe/Binarna Pretraga/Zadatak_1_Drugi_Nacin.cpp
+++ Vjezbe/Binarna Pretraga/Zadatak_1_Drugi_Nacin.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
+//Binarna pretraga
 
+int main() {
+    char niz[20];
+    cout << "Unesite znakove niza!\n";
+    for(int i = 0; i < 20; i++) {
+        cin >> niz[i];
+    }
+
+    sort(niz, niz + 20); //Program ne ispisuje poziciju znaka '@' ako ne sortiramo niz.
+
+    int poz(0);
+    bool pronadjen(false);
+    int l(0), d = 20 - 1;
+    while(l <= d) {
+        char s = (l + d) / 2;
+        if(niz[s] == '@') {
+            pronadjen = true;
+            poz = (int)s;
+            break;
+        }
+        else if('@' < niz[s]) {
+            d = s - 1;
+        }
+        else { 
+            l = s + 1;
+        }
+    }
+    if(pronadjen) cout << "Element @ se nalazi na poziciji: " << poz + 1;
+    else cout << "Element @ nije u nizu!";
     return 0;
 }
 /*
