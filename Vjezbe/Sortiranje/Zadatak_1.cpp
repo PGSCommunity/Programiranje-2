5ff122e75f4a12bb1fbc5c1aa84b31389e0e502b
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 15:02:31 +0200

    Vjezbe - Sortiranje - Zavrsen 1. zadatak

--- Vjezbe/Sortiranje/Zadatak_1.cpp
+++ Vjezbe/Sortiranje/Zadatak_1.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void unos(int n[5]) {
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < 5; i++) {
+        cin >> n[i];
+    }
+}
 
+void ispis(int niz[5]) {
+    cout << "Sortiran niz:\n";
+    for(int i = 0; i < 5; i++) {
+        cout << niz[i] << " ";
+    }
+}
+int main() {
+    int niz[5];
+    unos(niz);
+    for(int i = 0; i < 5; i++) {
+        for(int j = 0; j < 5 - 1 - i; j++) {
+            if(niz[j] > niz[j + 1]) {
+                int temp = niz[j + 1];
+                niz[j + 1] = niz[j];
+                niz[j] = temp;
+            }
+        }
+    }
+    ispis(niz);
     return 0;
 }
 /*
