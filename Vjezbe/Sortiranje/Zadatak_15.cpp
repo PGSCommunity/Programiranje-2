62a5b8a97b80bdc5fbd03085a8c1288bfbfcb70f
Danin Sadzak <daninsadzak@gmail.com>
2023-04-09 00:29:43 +0200

    Vjezba - Sortiranje - Postavljen 15. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_15.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Ucitati niz on n elemenata. Niz treba sortirati tako da se ispisuju prvo negativni, broj 0, pa pozitivni brojevi.
+Program treba da ima 3 funkcije: unos, sortiranje, i ispis.
+*/
