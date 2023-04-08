6b165b9acb0e4bdbe1bd920f32536d98a61a0817
Danin Sadzak <daninsadzak@gmail.com>
2023-04-09 00:11:30 +0200

    Vjezbe - Sortiranje - Zavrsen 13. zadatak

--- Vjezbe/Sortiranje/Zadatak_13.cpp
+++ Vjezbe/Sortiranje/Zadatak_13.cpp
@@ -2,7 +2,30 @@
 using namespace std;
 
 int main() {
-
+    int niz[8];
+    cout << "Unesite clanove niza!\n";
+    for(int i = 0; i < 8; i++) {
+        cin >> niz[i];
+    }
+    
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8 - 1 - i; j++) {
+            if(niz[j] > niz[j + 1]) {
+                int temp = niz[j + 1];
+                niz[j + 1] = niz[j];
+                niz[j] = temp;
+            }
+        }
+    }
+    cout << "Prvih 5 clanova sortirani uzlazno:\n";
+    for(int i = 0; i < 5; i++) {
+        cout << niz[i] << " ";
+    }
+    cout << endl;
+    cout << "Posljednja 3 clana sortirana silazno:\n";
+    for(int i = 8 - 1; i >= 5; i--) {
+        cout << niz[i] << " ";
+    }
     return 0;
 }
 /*
