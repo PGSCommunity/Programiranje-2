8c1e6bc2582c2753b73d73f679ae59fccda5b63c
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 18:39:20 +0200

    Vjezbe - Sortiranje - Postavljen 7. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Napisati program koji omogucava unos niza a od velicine n. Zatim se formira niz b od parnih brojeva iz niza a, pa se
+niz sortira u rastucem redoslijedu.
+*/
