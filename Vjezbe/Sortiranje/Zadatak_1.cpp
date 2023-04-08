720c698ff0b33de82728a2edc1f3563ff629b5ef
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 14:56:45 +0200

    Vjezbe - Sortiranje - Postavljen 1. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji sortira niz od 5 elemenata tako da sadrži i funkciju za unos, te funkciju za ispis elemenata 
+niza nakon sortiranja.
+*/
