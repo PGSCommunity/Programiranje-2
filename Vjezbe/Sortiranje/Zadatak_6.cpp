dbd7824aaa3e78a4f1c2ffa7cee5b3f67b4eb2c7
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 18:16:43 +0200

    Vjezbe - Sortiranje - Postavljen 6. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+U niz ucitavati realne brojeve sve dok im je aritmeticka sredina manja od 10 ili dok se ne popuni niz od 10 brojeva.
+Zatim sortirati brojeve koristeci bubble sort.
+*/
