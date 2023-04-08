0053c92ddc541768daf550d98aae4971dcb3eb02
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 02:15:10 +0200

    Vjezbe - Binarna Pretraga - Postavljen 2. zadatak

---
+++ Vjezbe/Binarna Pretraga/Zadatak_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji pronalazi element a u učitanom nizu dimenzije n, elementi niza se nalaze u strogo rastućem 
+poretku. Potom se računa aritmetička sredina elemenata manjih od broja a.(binarna pretraga)
+*/
