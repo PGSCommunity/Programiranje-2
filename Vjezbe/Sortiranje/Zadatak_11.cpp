1a6ede900615e98c0da94d783e8989e2577ee900
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 20:58:56 +0200

    Vjezbe - Sortiranje - Postavljen 11. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Ucitati strukturu pod nazivom ucenik(ime, prosjek). Sortirati ime i prosjek u opadajucem poretku i 
+ispisati ime ucenika sa najvecim prosjekom.
+*/
