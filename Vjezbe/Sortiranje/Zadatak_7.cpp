bd23fe61da3f811c3f13af688efacf00547f547e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 19:01:05 +0200

    Vjezbe - Sortiranje - Zavrsen 7. zadatak

--- Vjezbe/Sortiranje/Zadatak_7.cpp
+++ Vjezbe/Sortiranje/Zadatak_7.cpp
@@ -2,7 +2,33 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int a[n], b[n], br(0);
+    cout << "Unesite clanove niza a!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        if(a[i] % 2 == 0) {
+            b[br] = a[i];
+            br++;
+        }
+    }
     
+    for(int i = 0; i < br; i++) {
+        for(int j = 0; j < br - 1 - i; j++) {
+            if(b[j] > b[j + 1]) {
+                int temp = b[j + 1];    
+                b[j + 1] = b[j];
+                b[j] = temp;
+            }
+        }
+    }
+
+    cout << "Niz b:\n";
+    for(int i = 0; i < br; i++) {
+        cout << b[i] << " ";
+    }
     return 0;
 }
 /*
