05e9bdf2767f4b880943baaed7e72f5395ef0f3c
Danin Sadzak <daninsadzak@gmail.com>
2023-04-09 00:19:10 +0200

    Vjezbe - Sortiranje - Postavljen 14. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_14.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program gdje se treba ucitati niz od n elemenata. Zatim se formira novi niz od parnih clanova prvog niza i
+treba ih sortirati u rastucem poretku. Zatim treba se unjeti proizvoljni broj, koji treba bit paran. Treba vidjeti
+je li unjeti broj paran, i na kojoj poziciji se on nalazi.
+*/
