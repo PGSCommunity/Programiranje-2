a6f5a154e38f696d0c848a598a11b5a784a9889b
Danin Sadzak <daninsadzak@gmail.com>
2023-04-09 00:27:21 +0200

    Vjezbe - Sortiranje - Zavrsen 14. zadatak

--- Vjezbe/Sortiranje/Zadatak_14.cpp
+++ Vjezbe/Sortiranje/Zadatak_14.cpp
@@ -2,7 +2,64 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int niz[n];
+    int a[n], br(0);
+    cout << "Unesite clanove niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+        if(niz[i] % 2 == 0) {
+            a[br] = niz[i];
+            br++;
+        }
+    }
+    
+    for(int i = 0; i < br; i++) {
+        for(int j = 0; j < br - 1 - i; j++) {
+            if(a[j] > a[j + 1]) {
+                int temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
 
+    cout << "Sortiran niz:\n";
+    for(int i = 0; i < br; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+
+    int x;
+    cout << "Unesite broj!\n";
+    unos:cin >> x;
+    if(x % 2 != 0) {
+        cout << "Broj mora biti paran!\n";
+        goto unos;
+    }
+
+    int poz(0);
+    bool pronadjen(false);
+    int l(0), d = br - 1;
+    while(l <= d) {
+        int s = (l + d) / 2;
+        if(x == a[s]) {
+            pronadjen = true;
+            poz = s;
+            break;
+        }
+        else if(x < a[s]) {
+            d = s - 1;
+        }
+        else {
+            l = s + 1;
+        }
+    }
+
+    if(pronadjen) cout << "Broj " << x << " se nalazi u nizu na poziciji: " << poz + 1;
+    else cout << x << " nije u nizu!";
     return 0;
 }
 /*
