95c97c3415eaafb9a9b5cf305c061abc556d3aac
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 18:02:25 +0200

    Vjezbe - Sortiranje - Postavljen 5. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu 'ucenik' (ime, redni br. i br. telefona), zatim napisati program koji sadrži niz od tri učenika 
+i omogučava unos pa sortirati podatke prema svakom od navedenih članova strukture.
+*/
