6317895b942a5c757e2c8852ba70f2de99084bd2
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 23:46:36 +0200

    Vjezbe - Binarna Pretraga - Zavrsen 5. zadatak

--- Vjezbe/Binarna Pretraga/Zadatak_5.cpp
+++ Vjezbe/Binarna Pretraga/Zadatak_5.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
 
+    int poz(0);
+    bool pronadjen(false);
+    for(int i = 0; i < n; i++) {
+        if(niz[i] == 3) {
+            pronadjen = true;
+            poz = i;
+            break;
+        }
+    }
+
+    if(pronadjen) cout << "Niz sadrzi broj 3 i nalazi se na poziciji " << poz + 1;
+    else cout << "Niz ne sadrzi broj 3!";
     return 0;
 }
 /*
