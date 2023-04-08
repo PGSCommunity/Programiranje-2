9a4491f016dbeb1706134d32d4045062dbad742d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 19:17:47 +0200

    Vjezbe - Sortiranje - Postavljen 9. zadatak

---
+++ Vjezbe/Sortiranje/Zadatak_9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Izracunati sumu parnih i proizvod neparnih od n ucitanih brojeva.Parne sortirati u rastucem a neparne u opadajucem 
+poretku
+*/
