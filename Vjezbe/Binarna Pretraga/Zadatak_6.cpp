278e35fbe98a3b77197c6115d349e16adfbd836d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-08 23:51:20 +0200

    Vjezbe - Binarna Pretraga - Postavljen 6. zadatak

---
+++ Vjezbe/Binarna Pretraga/Zadatak_6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Ucitati niz on n elemenata i pronaci broj x u nizu koristeci binarnu pretragu. Zatim treba pronaci prvi parni broj
+manji od broja x.
+*/
