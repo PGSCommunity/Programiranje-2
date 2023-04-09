b6c827eec0f61da79fb2bc1de56613115fca0065
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 00:06:35 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 2. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+2.	Učitati niz znakova (string) pa ispisati:
+
+-	Učitani string sa pozdravnim porukama na početku i kraju stringa
+-	String sa umetnutom rijeci „PAUZA“ koja počinje od sredine stringa
+-	Obrisati predposljednju riječ stringa
+-	Zamijeniti savko pojavljivanje slova 'd' znakom '#'
+-	Poziciju drugog pojavljivanja učitane riječi (pretpostavimo da se riječ u tekstu pojavljuje više puta)
+
+*/
