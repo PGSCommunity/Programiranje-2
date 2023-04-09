b21c10cd7bfbf17b7da3f77d352235ec73ce2fff
Danin Sadzak <daninsadzak@gmail.com>
2023-04-09 18:00:45 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 1. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+1.	Učitati niz znakova (char) pa ispisati:
+
+-	Prva tri znaka stringa
+-	Znak ispod znaka u učitanom stringu
+-	Riječ ispod riječ
+-	Broj velikih i malih slova, broj interpunkcija
+-	Postring od 4 do 9 znaka
+-	Konvertovan 5. znak u integer i izračunat dvostruku vrijednost
+*/
