10840f0a6209d0519c6f2b512a1feedb3e78f03f
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 14:55:52 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 3. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+3.	Napisati program kojim se određuje
+-	Broj pojavljivanja stringa s1 u stringu s2
+-	Poziciju prvog pojavljivanja stringa s1 u stringu s2 počevši od pozicije k
+-	U datom stringu s zamjenjuje svako pojavljivanje stringa s1 stringom s2.
+*/
