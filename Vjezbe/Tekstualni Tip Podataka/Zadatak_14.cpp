1bda9265355fdd62e8bff11778283458cc09c31d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 19:27:54 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 14. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_14.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Ucitati niz znakova (string) pa:
+-umetnuti tekst po vasem izboru
+-zamjeniti tekst po vasem izboru
+-brisanje teksta po vasem izboru
+*/
