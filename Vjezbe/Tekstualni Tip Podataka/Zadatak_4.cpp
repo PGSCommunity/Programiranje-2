95d47f4b63778f52c3c8c5457dd3067461a0f930
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 16:33:42 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 4. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_4.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_4.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main() {
+    string s;
+    cout << "Unesite recenicu!\n";
+    getline(cin, s);
 
+    cout << "Svaka recenica u novom redu:\n";
+    for(int i = 0; i < s.size(); i++) {
+        cout << s[i];
+        if(ispunct(s[i]) && s[i] != ',') cout << endl;
+    }
     return 0;
 }
 /*
