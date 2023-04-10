9e3f8db736a9a2e496b7652f454fc7a97acf2784
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 19:49:35 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 15. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_15.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_15.cpp
@@ -1,10 +1,26 @@
-#include <iostream>
-using namespace std;
+    #include <iostream>
+    #include <string>
+    using namespace std;
 
-int main() {
+    int main() {
+        string str;
+        cout << "Unesite string!\n";
+        getline(cin, str);
 
-    return 0;
-}
+        int tmax(0), br(0);
+        for(int i = 0; i < str.size(); i++) {
+            if(str[i] == 'A') {
+                br++;
+            }
+            else {
+                if(tmax < br) tmax = br;
+                br = 0;
+            }
+        }
+        if(br > tmax) tmax = 0;
+        cout << "Najduza serija slova A ima " << tmax << " slova.";
+        return 0;
+    }
 /*
 Napisati program koji odredjuje duzinu najduze serije slova A u datom tekstu
 */
