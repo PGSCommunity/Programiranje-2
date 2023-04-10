cf10dbdd7ad5098eb4dab02b0927016c6c43d939
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 17:59:50 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 10. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+    Ucitati niz znakova pa ispisati:
+    1. Tekst sa umetnutom pozdravnom porukom na kraju (Zdravo)
+    2. Ispisati taj niz znakova tako da se svaki space mijenja u *
+    3. Substr od 10 do 20 znaka
+    4. Poz pojavljivanja druge rijeci u tekstu
+*/
