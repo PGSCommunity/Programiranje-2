a11c12ad9101564254da7dca1d9031d552d76a7a
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 17:35:42 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 7. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_7.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_7.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main() {
-
+    char c;
+    cout << "Unesite znak!\n";
+    cin >> c;
+    cout << "Njegov prethodnik: " << (char)(c - 1) << '\n';
+    cout << "Njegov sljedbenik: " << (char)(c + 1);
     return 0;
 }
 /*
