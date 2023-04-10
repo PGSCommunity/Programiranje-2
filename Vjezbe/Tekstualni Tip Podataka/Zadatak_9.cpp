cee5d99b6f7d653272b6dd1dac33a22d54ab2307
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 17:49:19 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 9. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_9.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_9.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <io.h>
+#include <fcntl.h>
+#include <stdio.h>
+
 using namespace std;
 
 int main() {
-
+    _setmode(_fileno(stdout), 0x00020000);
+    wcout << "Omega: " << L"\x03C9\n";
+    wcout << "Beskonacno: " << L"\x221E\n";
+    wcout << "Proizvod: " << L"\x041F";
     return 0;
 }
 /*
