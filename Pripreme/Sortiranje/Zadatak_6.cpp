144062ffd684013653edd5fecffbe187b9df99c7
Danin Sadzak <daninsadzak@gmail.com>
2023-04-11 01:28:44 +0200

    Pripreme - Sortiranje - Ispavljen 6. zadatak

--- Pripreme/Sortiranje/Zadatak_6.cpp
+++ Pripreme/Sortiranje/Zadatak_6.cpp
@@ -3,32 +3,17 @@
 using namespace std;
 
 struct Ucenik {
-    string ime_prezime;
+    string imePrezime;
     int razred;
     float prosjek;
 };
 
-int main() {
-    int n;
-    cout << "Koliko ucenika?\n";
-    cin >> n;
-    Ucenik u[n];
+void ispisUvjeta(Ucenik u[], int n) {
+    cout << "Svi ucenici drugog razreda:\n";
     for(int i = 0; i < n; i++) {
-        cin.ignore(); // sve praznine i karakteri novog reda ostaju u baferu, tako da će sljedeći poziv getline ispravno učitati string koji korisnik unosi.
-        cout << "Unesite ime i prezime " << i + 1 << ". ucenika!\n";
-        getline(cin, u[i].ime_prezime);
-        cout << "Unesite razred " << i + 1 << ". ucenika!\n";
-        cin >> u[i].razred;
-        cout << "Unesite prosjek " << i + 1 << ". ucenika!\n";
-        cin >> u[i].prosjek;
-        cout << "--------------------\n\n";
+        if(u[i].razred == 2) cout << u[i].imePrezime << " ";
     }
-
-    cout << "Svi ucenici 2. razreda:\n";
-    for(int i = 0; i < n; i++) {
-        if(u[i].razred == 2) cout << u[i].ime_prezime << " ";
-    }
-    cout << '\n';
+    cout << endl;
     float s(0), br(0);
     for(int i = 0; i < n; i++) {
         if(u[i].razred == 1) {
@@ -36,32 +21,27 @@ int main() {
             br++;
         }
     }
-    cout << "Prosjek ucenika prvih razreda: " << s/br << '\n';
-    cout << "Imena i prezima ucenika sa prosjekom 5.0:\n";
+    cout << "Prosjecna ocjena ucenika prvog razreda: " << s / br << '\n';
+    cout << "Imena i prezimena ucenika sa prosjekom 5.0:\n";
     for(int i = 0; i < n; i++) {
-        if(u[i].prosjek == 5.0) cout << u[i].ime_prezime << " ";
+        if(u[i].prosjek == 5.0) cout << u[i].imePrezime << " ";
     }
-    cout << endl << endl;
+    cout << endl << '\n';
+}
+
+void sortiranje(Ucenik u[], int n) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n - 1 - i; j++) {
-            if(u[j].ime_prezime > u[j + 1].ime_prezime) {
-                string temp = u[j + 1].ime_prezime;
-                u[j + 1].ime_prezime = u[j].ime_prezime;
-                u[j].ime_prezime = temp;
+            if(u[j].imePrezime > u[j + 1].imePrezime) {
+                string temp = u[j + 1].imePrezime;
+                u[j + 1].imePrezime = u[j].imePrezime;
+                u[j].imePrezime = temp;
             }
-        }
-    }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n - 1 - i; j++) {
             if(u[j].razred > u[j + 1].razred) {
                 int temp = u[j + 1].razred;
                 u[j + 1].razred = u[j].razred;
                 u[j].razred = temp;
             }
-        }
-    }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n - 1 - i; j++) {
             if(u[j].prosjek > u[j + 1].prosjek) {
                 float temp = u[j + 1].prosjek;
                 u[j + 1].prosjek = u[j].prosjek;
@@ -69,22 +49,41 @@ int main() {
             }
         }
     }
+}
 
-    cout << "Sortirana imena i prezimena ucenika:\n";
+int main() {
+    int n;
+    cout << "Koliko ucenika?\n";
+    cin >> n;
+
+    Ucenik u[n];
     for(int i = 0; i < n; i++) {
-        cout << u[i].ime_prezime << " ";
+        cin.ignore();
+        cout << "Unesite ime i prezime " << i + 1 << ". ucenika!\n";
+        getline(cin, u[i].imePrezime);
+        cout << "Unesite razred " << i + 1 << ". ucenika!\n";
+        cin >> u[i].razred;
+        cout << "Unesite prosjek " << i + 1 << ". ucenika!\n";
+        cin >> u[i].prosjek;
+        cout << "====================\n\n";
     }
-    cout << '\n';
-    cout << "Sortirani razredi ucenika:\n";
+
+    ispisUvjeta(u, n);
+
+    sortiranje(u, n);
+    cout << "Sortirana struktura:\n";
+    for(int i = 0; i < n; i++) {
+        cout << u[i].imePrezime << " ";
+    }
+    cout << endl;
     for(int i = 0; i < n; i++) {
         cout << u[i].razred << " ";
     }
-    cout << '\n';
-    cout << "Sortirani prosjeci ucenika:\n";
+    cout << endl;
     for(int i = 0; i < n; i++) {
         cout << u[i].prosjek << " ";
     }
-    return 0;
+    return 0;   
 }
 /*
 6.	Kreirati strukturu podataka Ucenik( ime_prezime, razred, prosjek). Sortirati podatke prema svakom od navedenih 
