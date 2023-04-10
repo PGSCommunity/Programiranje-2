0e739490ac0a2f49c420c8ab45d547616c5fe60b
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 19:17:41 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 12. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_12.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_12.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string str;
+    cout << "Unesite string!\n";
+    getline(cin, str);
 
+    cout << "Slovo a je nalazi na:\n";
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == 'a' || str[i] == 'A') {
+            cout << i + 1 << ". mjestu\n";
+        }
+    }
     return 0;
 }
 /*
