80d5ba99dfbca0a0d9b4d61b66d072e7a97edf78
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 16:27:51 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 4. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+4.	Napisati program koji svaku rečenicu iz ulaznog toka ispisuje u zasebnom redu. Pod rečenicom se podratumijeva niz znakova koji 
+završava sa zanakovima '!', '?' ili '.'
+*/
