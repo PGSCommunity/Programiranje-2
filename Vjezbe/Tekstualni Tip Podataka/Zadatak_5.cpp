aac27d3cef7c3fb814798d8d9f361179635c1c7a
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 16:37:08 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 5. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+5.	String promjenjiva time čuva vrijeme u formatu „ss:mm“ gdje je ss – broj sati, a mm – broj minuta, npr. '17:45'. 
+Napisati program koji na osnovu zadatog vremena time računaj koliko je minuta proteklo od ponoći.
+*/
