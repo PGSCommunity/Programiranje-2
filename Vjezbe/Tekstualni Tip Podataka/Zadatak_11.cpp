459b51276592b6cb5a2a9d06d47957736b27ae62
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 19:05:30 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 11. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Napisati funkciju koja vraca poziciju prvog pojavljivanja stringa s2 u stringu s1. Ako se string s2 ne nalazi u stringu s1 funkcija
+kao rezultat vraca -1
+*/
