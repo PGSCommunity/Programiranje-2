6695a08bba79fb8ca3b5b71b1a463ce4aa3c7b40
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 19:24:04 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 13. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_13.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_13.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string str;
+    cout << "Unesite string!\n";
+    getline(cin, str);
+    char c;
+    cout << "Unesite znak!\n";
+    cin >> c;
 
+    int br(0);
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == c) br++;
+    }
+
+    if(br > 0) cout << "Znak " << c << " se ponavlja " << br << " puta.";
+    else cout << "Znak " << c << " nije u stringu!";
     return 0;
 }
 /*
