5c512e2727fe7b31ec171c2090d31fc626778d77
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 19:38:41 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 14. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_14.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_14.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string str;
+    cout << "Unesite string!\n";
+    getline(cin, str);
 
+    string str1, str2;
+    
+    cout << "Unesite rijec sa umetanje!\n";
+    getline(cin, str1);
+    int a;
+    cout << "Na kojoj poziciji?\n";
+    cin >> a;
+    cout << "Teksta sa umetnutnim tekstom:\n";
+    cout << str.insert(a, str1);
+    cout << endl << '\n';
+
+    cout << "Unesite tekst za zamnjenu!\n";
+    cin.ignore();
+    getline(cin, str2);
+    int b, c;
+    cout << "Unesite pocetnu poziciji i broj znakova za zamjenu!\n";
+    cin >> b >> c;
+    cout << "Tekst za zamjenom teksta:\n";
+    cout << str.replace(b, c, str2);
+    cout << endl << '\n';
+
+    int d, e;
+    cout << "Unesite pocetnu poziciji i broj znakova za brisanje!\n";
+    cin >> d >> e;
+    cout << "Teksta za izbrisanim tekstom:\n";
+    cout << str.erase(d, e);
     return 0;
 }
 /*
