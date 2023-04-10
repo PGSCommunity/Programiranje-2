4411799db93f05e183df556c0aa392eac71dfb27
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 17:27:01 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 6. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_6.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_6.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string s;
+    cout << "Unesite recenicu:\n";
+    getline(cin, s);
+    string str(s.length(), ' ');
+    for(int i = 0; i < s.size(); i++) {
+        str[i] = toupper(s[i]);
+    }
 
+    int brA(0), brO(0);
+    for(int i = 0; i < str.size(); i++) {
+        if(str[i] == 'A') brA++;
+        if(str[i] == 'O') brO++;
+    }
+    if(brA >= brO) cout << str << " nije pristojna recenica!\n";
+    else cout << str << " je pristojna recenica!\n";
     return 0;
 }
 /*
