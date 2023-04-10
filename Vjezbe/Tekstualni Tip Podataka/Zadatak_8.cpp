6e145338e4d530e0e9c6678f8a56b4f689995a0c
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 17:37:10 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 8. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Upotrebom aritmetike (koristeći ASCII tablicu) napisati program koji za uneseno veliko slovo ispisuje malo slovo.
+*/
