b3f5e5e77f637b1005967a459ff9f695a0a685ea
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 18:11:26 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 10. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_10.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_10.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string str;
+    cout << "Unesite recenicu!\n";
+    getline(cin, str);
+    
+    string str1(str), poruka("Zdravo");
+    cout << "Tekst sa umetnutom pozdravnom porukom na kraju:\n";
+    cout << str1.append(poruka);
+    cout << endl << '\n';
 
+    string str2(str);
+    for(int i = 0; i < str.size(); i++) {
+        if(isspace(str2[i])) str2[i] = '*';
+    }
+    cout << "Ispis teksta tako da se svaki space mijenja u '*':\n";
+    cout << str2;
+    cout << endl << '\n';
+
+    if(str.size() >= 20) {
+        cout << "Substring teksta od 10. do 20. znaka:\n";
+        cout << str.substr(10, 11);
+        cout << endl << '\n';
+    }
+    else {
+        cout << "String je manji od 20 znakova!\n";
+        cout << endl << '\n';
+    }
     return 0;
 }
 /*
