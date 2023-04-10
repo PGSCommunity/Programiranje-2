783055688dc588ad8a5e5e69b69d8a088a247df2
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 17:13:44 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 6. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+6.	U nekoj zemlji X „pristojnim“ se smatraju rečenice u kojima broj slova A nije veći od broja slova O. Napisati program koji 
+provjerava da li je rečenica „pristojna“. Npr. „DOBAR DAN“ je u zemlji X nepristojna, a „DOZVOLJENO KOKODAKANJE“ pristojna rečenica.
+*/
