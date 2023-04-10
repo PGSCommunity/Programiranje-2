15dd2289320374373d4bb304a80a5f1f58fd91b5
Danin Sadzak <daninsadzak@gmail.com>
2023-04-10 19:11:34 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 11. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_11.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_11.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int poz(string s1, string s2) {
+    if(s1.find(s2)) return s1.find(s2);
+    else return -1;
+}
+
 int main() {
-    
+    string s1, s2;
+    cout << "Unesite prvi string!\n";
+    getline(cin, s1);
+    cout << "Unesite drugi string!\n";
+    getline(cin, s2);
+
+    cout << poz(s1, s2);
     return 0;
 }
 /*
