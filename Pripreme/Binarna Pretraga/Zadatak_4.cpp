85f691ef0e2b878a12f601f5a97d0748608dc99a
Danin Sadzak <daninsadzak@gmail.com>
2023-04-12 00:37:51 +0200

    Pripreme - Binarna Pretraga - Zavrsen 4. zadatak

--- Pripreme/Binarna Pretraga/Zadatak_4.cpp
+++ Pripreme/Binarna Pretraga/Zadatak_4.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
+int faktorijel(int niz) {
+    if(niz == 0 || niz == 1) return 1;
+    return niz * (niz - 1);
+}
 
+int main() {
+    int n, x;
+    cout << "Velicina niza?\n";
+    cin >> n;
+
+    int niz[n];
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+
+    sort(niz, niz + n);
+
+    cout << "Unesite broj x!\n";
+    cin >> x;
+
+    int poz(0);
+    bool pronadjen(false);
+    int l(0), d = n - 1;
+    while(l <= d) {
+        int s = (l + d) / 2;
+        if(x == niz[s]) {
+            poz = s;
+            pronadjen = true;
+            break;
+        }
+        else if(x < niz[s]) {
+            d = s - 1;
+        }
+        else {
+            l = s + 1;
+        }
+    }
+
+    if(pronadjen) cout << "Broj se nalazi na poziciji " << poz + 1 << '\n';
+    else cout << "Broj nije u nizu!\n";
+    cout << endl;
+
+    for(int i = 0; i < poz; i++) {
+        int fakt = faktorijel(niz[i]);
+        cout << "Faktorijel broja " << niz[i] << " je: " << fakt << '\n';
+    } 
     return 0;
 }
 /*
