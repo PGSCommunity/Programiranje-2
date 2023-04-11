0c3e9a8b5c00006945d9ec1d176d71efb357a84c
Danin Sadzak <daninsadzak@gmail.com>
2023-04-12 00:26:11 +0200

    Pripreme - Binarna Pretraga - Postavljen 4. zadatak

---
+++ Pripreme/Binarna Pretraga/Zadatak_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+3.	Napisati program koji omogućava deklaraciju proizvoljnog niza brojeva i unos prirodnog broja x (koji oznacava poziciju nekog 
+broja u nizu), a zatim ispisuje faktorijele svih brojeva niza do broja x (uključujući i ta dva). Pri rješavanju zadatka kreirati 
+funkciju "faktorijel", koja računa faktorijel primljenog broja. (varijacija: omogučiti korisničko definiranje niza i unosa broja 
+kojem treba odrediti poziciju).
+*/
