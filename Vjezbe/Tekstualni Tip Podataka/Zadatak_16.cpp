81e9fa4019504606f6cce27c17ab547c3421e536
Danin Sadzak <daninsadzak@gmail.com>
2023-04-12 20:47:05 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 16. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_16.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_16.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main() {
-
+    
     return 0;
 }
 /*
-Napisati program koji nakon unosa recenice (char), program ispisuje broj malih, velikih slova i interpunkcija.
+Napisati program koji nakon unosa recenice (char), program ispisuje broj malih i velikih slova. Nesmiju se koristiti funkcije
+(isupper i islower).
 */
