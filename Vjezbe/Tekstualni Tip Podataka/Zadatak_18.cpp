28b3fb3a981adf88d7257dd1d04282f84b7a5073
Danin Sadzak <daninsadzak@gmail.com>
2023-04-12 20:58:49 +0200

    Vjezbe - Tekstualni Tip Podataka - Postavljen 18. zadatak

---
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_18.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program u kojem nakon unosa teksta (string), program racuna broj recenica u tekstu. (Recenica se podrazumijeva kao se 
+zavrsava na . ? ili !).
+*/
