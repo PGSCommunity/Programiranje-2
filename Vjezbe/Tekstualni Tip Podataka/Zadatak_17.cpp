f5a9903fe158b433cccffd9cc00f04290cd25f82
Danin Sadzak <daninsadzak@gmail.com>
2023-04-12 20:57:01 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 17. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_17.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_17.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
+    string str;
+    cout << "Unesite tekst!\n";
+    getline(cin, str);
 
+    int br(0);
+    for(int i = 0; i < str.size(); i++) {
+        if(isspace(str[i])) br++;
+    }
+
+    cout << "Broj rijeci u tekstu: " << br + 1;
     return 0;
 }
 /*
