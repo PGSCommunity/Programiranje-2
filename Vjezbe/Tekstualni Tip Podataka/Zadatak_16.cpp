4b31ab747c398cfcab0a8b507061420fb3d930d5
Danin Sadzak <daninsadzak@gmail.com>
2023-04-12 20:51:32 +0200

    Vjezbe - Tekstualni Tip Podataka - Zavrsen 16. zadatak

--- Vjezbe/Tekstualni Tip Podataka/Zadatak_16.cpp
+++ Vjezbe/Tekstualni Tip Podataka/Zadatak_16.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
-    
+    char c[100];
+    cout << "Unesite tekst!\n";
+    cin.getline(c, 100);
+
+    int br1(0), br2(0);
+    for(int i = 0; i < strlen(c); i++) {
+        if(c[i] >= 65 && c[i] <= 90) br1++;
+        if(c[i] >= 95 && c[i] <= 122) br2++;
+    }
+
+    cout << "Broj velikih slova: " << br1 << '\n';
+    cout << "Broj malih slova: " << br2;
     return 0;
 }
 /*
