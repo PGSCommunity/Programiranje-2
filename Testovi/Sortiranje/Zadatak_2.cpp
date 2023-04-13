b5378bb85a10021b558e9caf0d3d769d6ddf5ffa
Danin Sadzak <daninsadzak@gmail.com>
2023-04-13 19:55:54 +0200

    Testovi - Sortiranje - Postavljen 2. zadatak

---
+++ Testovi/Sortiranje/Zadatak_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definisati strukturu Hidrometeorolska (godisnje_doba, temperatura, padavine).
+Koristenjem funkcija nakon ucitavanja podataka ispisati:
+- Podatke sortirane prema kolicini padavina
+- Prosjecnu temperaturu za godisnje doba ljeto (prepostavlja se da je uneseno vise razlicitih vrijednosti za jedno godisnje doba)
+- Godisnje doba i temperaturu prvu manju od korisnicki unesene
+*/
