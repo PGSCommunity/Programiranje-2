2733c247f87ef43c6aa16640c6e4b305121690e9
Danin Sadzak <daninsadzak@gmail.com>
2023-04-13 19:20:01 +0200

    Testovi - Binarna Pretraga - Postavljen 1. zadatak

---
+++ Testovi/Binarna Pretraga/Zadatak_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Koristeci funkcije napisati program pomocu kojeg je potrebno ucitati n brojeva u polje i nakon toga broj A koji se trazi u polju.
+Ako se broj pojavljuje u polju, izracunati i ispisati broj parnih elemenata manjih od broja A (Koristiti binarnu pretragu).
+*/
