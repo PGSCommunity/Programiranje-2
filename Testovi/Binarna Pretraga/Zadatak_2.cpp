0495734aee6233d9315db808de65e3e119031c2e
Danin Sadzak <daninsadzak@gmail.com>
2023-04-13 19:35:13 +0200

    Testovi - Binarna Pretraga - Postavljen 2. zadatak

---
+++ Testovi/Binarna Pretraga/Zadatak_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Koristeci funkcije napisati program pomocu kojeg je potrebno ucitati s brojeva u polje i nakon toga broj x koji se trazi u polju.
+Ako se broj pojavljuje u polju, ispisati poziciju pojavljivanja prvog parnog brojav veceg od ucitanog broja x (Koristit binarnu
+pretragu).
+*/
