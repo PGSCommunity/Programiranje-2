dfa4492b953fcd22defd0916fac6093805bd18a6
Danin Sadzak <daninsadzak@gmail.com>
2023-04-13 19:44:56 +0200

    Testovi - Sortiranje - Postavljen 1. zadatak

---
+++ Testovi/Sortiranje/Zadatak_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Koristeci funkcije potrebno je ucitati a clanova niza, a zatim izracunati i ispisati one clanove niza koji nisu djeljivi sa 3 u
+rastucem poretku.
+*/
