c69a609190e323d3624514d5fd90ac1595146430
Danin Sadzak <daninsadzak@gmail.com>
2023-04-13 19:42:14 +0200

    Testovi - Binarna Pretraga - Zavrsen 2. zadatak

--- Testovi/Binarna Pretraga/Zadatak_2.cpp
+++ Testovi/Binarna Pretraga/Zadatak_2.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int main() {
+void upis(int niz[], int n) {
+    cout << "Unesite elemente niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+    }
+}
 
+bool binarna(int niz[], int n, int x, int &poz) {
+    int l(0), d = n - 1;
+    while(l <= d) {
+        int s = (l + d) / 2;
+        if(x == niz[s]) {
+            poz = s;
+            return true;
+        }
+        else if(x < niz[s]) {
+            d = s - 1;
+        }
+        else {
+            l = s + 1;
+        }
+    }
+    return false;
+}
+
+void prviParni(int niz[], int poz, int n) {
+    for(int i = poz; i < n; i++) {
+        if(niz[i] % 2 == 0) {
+            cout << "Prvi parni broj veci od pozicijie: " << niz[i];
+            break;
+        }
+    }
+}
+
+int main() {
+    int S, X;
+    cout << "Velicina niza?\n";
+    cin >> S;
+
+    int niz[S];
+    upis(niz, S);
+
+    cout << "Unesite broj x!\n";
+    cin >> X;
+
+    int poz(0);
+    if(binarna(niz, S, X, poz)) {
+        cout << "Broj se nalazi na poziciji: " << poz + 1 << '\n';
+    } else {
+        cout << "Broj nije u nizu!\n";
+    }
+    
+    prviParni(niz, poz, S);
     return 0;
 }
 /*
