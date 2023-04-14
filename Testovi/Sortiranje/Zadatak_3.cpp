58891d208dd3df95a24e2507f93bf87c2a0b1624
Danin Sadzak <daninsadzak@gmail.com>
2023-04-14 20:33:28 +0200

    Testovi - Sortiranje - Postavljen 3. zadatak

---
+++ Testovi/Sortiranje/Zadatak_3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Koristeci funkcije potrebno je ucitati n clanova niza, a zatim izracunati i ispisati clanove niza koji su djeljivi i sa 2 i sa 5 u
+rastucem poretku
+*/
