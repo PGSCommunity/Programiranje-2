37a65a959fa31c54753ce51be81debc8e705b3c7
Danin Sadzak <daninsadzak@gmail.com>
2023-04-18 02:49:22 +0200

    Testovi - Sortiranje - Postavljen 4. zadatak

---
+++ Testovi/Sortiranje/Zadatak_4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definisati strukturu Zaposlenik (ime, sektor, plata). Koristenjem funkcija nakon ucitavanja podataka ispisati:
+- podatke sortirane prema visini plate - u rastucem redoslijedu
+- prosjecnu platu u IT sektoru 
+- imena zaposlenika u opadajucem poretku i pripadajuci sektor koji imaju platu manju od korisnicki unesene
+*/
