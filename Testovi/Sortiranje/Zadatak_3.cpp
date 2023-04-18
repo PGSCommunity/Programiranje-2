8120a818510efdcf773939ee517dd6a66256c627
Danin Sadzak <daninsadzak@gmail.com>
2023-04-18 02:46:36 +0200

    Testovi - Sortiranje - Zavrsen 3. zadatak

--- Testovi/Sortiranje/Zadatak_3.cpp
+++ Testovi/Sortiranje/Zadatak_3.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void upis(int niz[], int n, int a[], int &br) {
+    cout << "Unesite clanove niza!\n";
+    for(int i = 0; i < n; i++) {
+        cin >> niz[i];
+        if(niz[i] % 2 == 0 && niz[i] % 5 == 0) {
+            a[br] = niz[i];
+            br++;
+        }
+    }
+}
 
+void sortiranje(int a[], int br) {
+    for(int i = 0; i < br; i++) {
+        for(int j = 0; j < br - 1 - i; j++) {
+            if(a[j] > a[j + 1]) {
+                int temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+ 
+void ispis(int a[], int br) {
+    cout << "Sortiran niz:\n";
+    for(int i = 0; i < br; i++) {
+        cout << a[i] << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    cin >> n;
+
+    int niz[n], a[n];
+    int br(0);
+
+    upis(niz, n, a, br);
+    sortiranje(a, br);
+    ispis(a, br);
     return 0;
 }
 /*
