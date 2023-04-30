a4cabf29f739b6db0c047a1fb8ac47df4b40e1a9
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 15:43:04 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 6. zadatak

--- Vjezbe/Ponavljanje/Petlje/6.cpp
+++ Vjezbe/Ponavljanje/Petlje/6.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
+    int a;
+    cout << "Unesite stranicu a:\n";
+    cin >> a;
 
+    int i(1), o(0), p(0);
+    do {
+        o = 3 * a;
+        p = (a * sqrt(3)) / 4;
+        cout << i << ". obim: " << o << '\n';
+        cout << i << ". povrsina: " << p << "\n\n";
+        i++;
+    } while(i <= 5);
     return 0;
 }
 /*
