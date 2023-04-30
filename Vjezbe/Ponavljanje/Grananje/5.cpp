aa753e27a1ab682ae03de9413440af3b3b717845
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:21:04 +0200

    Vjezba - Ponavljanje (Grananje) - Zavrsen 5. zadatak

--- Vjezbe/Ponavljanje/Grananje/5.cpp
+++ Vjezbe/Ponavljanje/Grananje/5.cpp
@@ -2,6 +2,20 @@
 using namespace std;
 
 int main() {
+    int x, y(0);
+    cout << "Unesite broj x!\n";
+    cin >> x;
+
+    if(x > 0) {
+        y = x + 3;
+        cout << "y: " << y;
+    } else if(x == 0) {
+        y = 2;
+        cout << "y: " << y;
+    } else {
+        y = x - 3;
+        cout << "y: " << y;
+    }
     return 0;
 }
 /*
