15b61a6a291c2bddfbb40a8d59d18d41335175aa
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:03:52 +0200

    Vjezbe - Ponavljanje (Grananje) - Zavrsen 2. zadatak

--- Vjezbe/Ponavljanje/Grananje/2.cpp
+++ Vjezbe/Ponavljanje/Grananje/2.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main() {
+    int a;
+    cout << "Unesite broj!\n";
+    unos:cin >> a;
+    if(a == 0) {
+        cout << "Broj mora bit razlicit on nule!\n";
+        goto unos;
+    }
 
+    if(a > 100) cout << "Broj je veci od 100!";
+    else cout << "Broj je manji od 100!";
     return 0;
 }
 /*
