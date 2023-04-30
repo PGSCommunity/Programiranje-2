528c0c31ddbdcf1ad92da70522b75a6679ef8c20
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:08:04 +0200

    Vjezbe - Ponavljanje (Grananje) - Zavrsen 3. zadatak

--- Vjezbe/Ponavljanje/Grananje/3.cpp
+++ Vjezbe/Ponavljanje/Grananje/3.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
+    int a;
+    cout << "Unesite broj a!\n";
+    unos:cin >> a;
+    if(a == 0) {
+        cout << "Broj mora bit razlicit od nule!\n";
+        goto unos;
+    }
 
+    if(a % 2 != 0) cout << "Dvostruka vrijednost unesenog broja: " << a * 2;
+    else cout << "Kvadratni korijen unesenog broja: " << sqrt(a);
     return 0;
 }
 /*
