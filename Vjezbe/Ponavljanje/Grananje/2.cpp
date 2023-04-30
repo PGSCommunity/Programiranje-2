e12f3489ce5847b9e7598a9615b0623a077077c2
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:01:44 +0200

    Vjezbe - Ponavljanje (Grananje) - Postavljen 2. zadatak

---
+++ Vjezbe/Ponavljanje/Grananje/2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji omogućava unos realnog broja različitog od 0,  te ispisuje poruku da li je broj veći ili manji od 100
+*/
