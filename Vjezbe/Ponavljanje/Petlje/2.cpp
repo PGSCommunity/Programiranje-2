ad94cb5abf1bac98bdbdf2088363a7a42bb43f3d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 15:20:49 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 2. zadatak

--- Vjezbe/Ponavljanje/Petlje/2.cpp
+++ Vjezbe/Ponavljanje/Petlje/2.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main() {
+    int k, l;
+    cout << "Unesite broj k i l\n";
+    cin >> k >> l;
 
+    int br(0);
+    for(int i = k; i <= l; i++) {
+        if(i % 2 == 0) br++;
+    }
+    
+    cout << "Broj parnih brojeva u intervalu: " << br;
     return 0;
 }
 /*
