7ae99c48e00a723141c85c6b348b5794878e023d
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:43:35 +0200

    Vjezbe - Ponavljanje (Grananje) - Zavrsen 6. zadatak

--- Vjezbe/Ponavljanje/Grananje/6.cpp
+++ Vjezbe/Ponavljanje/Grananje/6.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    int a, b, c;
+    cout << "Unesite stranice a,b i c!\n";
+    cin >> a >> b >> c;
+
+    if(a == b && a == c && b == c) cout << "Trougao je istostranicni!";
+    else if(a == b && c > a) cout << "Trougao je pravougli!";
+    else cout << "Trougao je raznostranicni!";
     return 0;
 }
 /*
