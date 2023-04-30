df39e7bd564909d826e296093313cbbfc70699f8
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:05:03 +0200

    Vjezbe - Ponavljanje (Grananje) - Postavljen 3. zadatak

---
+++ Vjezbe/Ponavljanje/Grananje/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji omogućava unos broja različitog od 0, ukoliko je broj neparan program računa i ispisuje njegovu dvostruku 
+vrijednost, u suprotnom računa i ispisuje njegov kvadratni korijenen
+*/
