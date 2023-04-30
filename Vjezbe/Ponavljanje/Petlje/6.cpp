946f4bf6dd46d6e2dc5c78f15204a419caaf2476
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 15:38:23 +0200

    Vjezbe - Ponavljanje (Petlje) - Postavljen 6. zadatak

---
+++ Vjezbe/Ponavljanje/Petlje/6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji računa površinu i obim pet jednakostraničnih trouglova za unesenu stranicu
+*/
