2a41bb66be222d8ae7ae36a98c56d273ec265d25
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 03:10:34 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 1. zadatak

--- Vjezbe/Ponavljanje/Petlje/1.cpp
+++ Vjezbe/Ponavljanje/Petlje/1.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main() {
-
+    int s(0);
+    for(int i = 0; i <= 100; i += 2) {
+        s += i;
+    }
+    cout << "Suma prvih 100 parnih brojeva: " << s;
     return 0;
 }
 /*
