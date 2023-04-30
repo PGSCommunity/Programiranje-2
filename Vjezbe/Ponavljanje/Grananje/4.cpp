3a60d03812f4f10b4e9eb2a2594743cba474a17b
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:10:21 +0200

    Vjezbe - Ponavljanje (Grananje) - Postavljen 4. zadatak

---
+++ Vjezbe/Ponavljanje/Grananje/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Napisati program koji omogućava unos parametara za proračun površine (zapremine) nekog geometriskoh tijela. Ukoliko su parametri 
+veći od 10, program računa i ispisuje površinu zadanog tijela, u suprotnom računa i ispisuje apsolutnu vrijednost zapremine tijela.
+*/
+//Editor's note: Za ovaj zadatak je odabrana kocka.
