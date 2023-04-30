136187969f4a55f309157f4506abfabc33db02d6
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:16:17 +0200

    Vjezba - Ponavljanje (Grananje) - Postavljen 5. zadatak

---
+++ Vjezbe/Ponavljanje/Grananje/5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Napisati program koji racuna x po formuli:
+    { x + 3, x > 0
+y = { 2,     x = 0
+    { x - 3, x < 0
+*/
