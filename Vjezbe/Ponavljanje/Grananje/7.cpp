de35abf97015f7dda300db2cc39be311125c9d56
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 02:46:17 +0200

    Vjezbe - Ponavljanje (Grananje) - Postavljen 7. zadatak

---
+++ Vjezbe/Ponavljanje/Grananje/7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program za ispis uspjeha, prema slijedećoj tabeli:
+1 - Nedovoljan
+2 - Dovoljan
+3 - Dobar
+4 - Vrlodobar
+5 - Odlican
+*/
