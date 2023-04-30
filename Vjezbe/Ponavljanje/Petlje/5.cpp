17eb548438778a92f659cd38a89196ff0ad99ce7
Danin Sadzak <daninsadzak@gmail.com>
2023-04-30 15:32:38 +0200

    Vjezbe - Ponavljanje (Petlje) - Postavljen 5. zadatak

---
+++ Vjezbe/Ponavljanje/Petlje/5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji računa sumu prirodnih brojeva u intervalu od k do n koji nisu djeljivi sa a.
+*/
