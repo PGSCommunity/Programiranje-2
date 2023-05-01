a3c462a20472ade468baa28a6bea2586fed46374
Danin Sadzak <daninsadzak@gmail.com>
2023-05-01 17:33:32 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 11. zadatak

--- Vjezbe/Ponavljanje/Petlje/11.cpp
+++ Vjezbe/Ponavljanje/Petlje/11.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main() {
+    int n;
+    int br(0);
+    cout << "Unesite broj:\n";
+    cin >> n;
 
+    do {
+        br++;
+        n /= 10;
+    } while(n != 0);
+    cout << "Broj cifara unesenog broja je: " << br;
     return 0;
 }
 /*
