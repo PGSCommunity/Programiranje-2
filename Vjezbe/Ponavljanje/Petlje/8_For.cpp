389bda31006bdc7b61f4b08932a89e3f1d94bce8
Danin Sadzak <daninsadzak@gmail.com>
2023-05-01 16:40:49 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 8. zadatak (For petlja)

--- Vjezbe/Ponavljanje/Petlje/8_For.cpp
+++ Vjezbe/Ponavljanje/Petlje/8_For.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main() {
-
+    int s(0);
+    for(int i = 15; i >= 5; i--) {
+        if(i % 2 == 0) s += i;
+    }
+    cout << "Suma parnih brojeva iz intervala: " << s;
     return 0;
 }
 /*
