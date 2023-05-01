30426012575c48915ba5fe574ca83a775b3692a6
Danin Sadzak <daninsadzak@gmail.com>
2023-05-01 17:03:25 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 9. zadatak (While petlja)

--- Vjezbe/Ponavljanje/Petlje/9_While.cpp
+++ Vjezbe/Ponavljanje/Petlje/9_While.cpp
@@ -2,7 +2,18 @@
 using namespace std;
 
 int main() {
+    int n, a, p(1);
+    int i(1);
+    cout << "Do broja?\n";
+    cin >> n;
+    cout << "Unesite broj a:\n";
+    cin >> a;
 
+    while(i <= n) {
+        if(i % a != 0) p *= i;
+        i++;
+    }
+    cout << "Proizvod brojeva koji nisu djeljivi sa " << a << " u intervalu: " << p;
     return 0;
 }
 /*
