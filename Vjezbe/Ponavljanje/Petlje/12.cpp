7b0047ed3a81db3562125c759edef9bb015fcf05
Danin Sadzak <daninsadzak@gmail.com>
2023-05-01 17:37:45 +0200

    Vjezbe - Ponavljanje - Zavrsen 12. zadatak

--- Vjezbe/Ponavljanje/Petlje/12.cpp
+++ Vjezbe/Ponavljanje/Petlje/12.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main() {
-
+    int s(0), br;
+    for(int i = 1; i <= 10; i++) {
+        cout << "Unesite " << i << ". broj!\n";
+        cin >> br;
+        s += br;
+    }
+    cout << "Suma unesenih brojeva: " << s;
     return 0;
 }
 /*
