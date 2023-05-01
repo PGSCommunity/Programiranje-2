261a37644e96b9b763d30da58d6151cd49f11663
Danin Sadzak <daninsadzak@gmail.com>
2023-05-01 17:13:16 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 10. zadatak (For petlja)

--- Vjezbe/Ponavljanje/Petlje/10_For.cpp
+++ Vjezbe/Ponavljanje/Petlje/10_For.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-
+    float d;
+    float a(0), o(0), p(0);
+    for(int i = 1; i <= 7; i++) {
+        cout << "Unesite " << i << ". dijagonalu:\n";
+        cin >> d;
+        a = (d * sqrt(2)) / 2;
+        p = pow(a, 2);
+        o = 4 * a;
+        cout << i << ". povrsina: " << p << '\n';
+        cout << i << ". obim: " << o << '\n';
+        cout << "====================\n\n"; 
+    }
     return 0;
 }
 /*
