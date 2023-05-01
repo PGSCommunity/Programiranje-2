74929b8bde4a6bb841366d7af2a18fc828cc53a1
Danin Sadzak <daninsadzak@gmail.com>
2023-05-01 17:44:30 +0200

    Vjezbe - Ponavljanje (Petlje) - Zavrsen 13. zadatak

--- Vjezbe/Ponavljanje/Petlje/13.cpp
+++ Vjezbe/Ponavljanje/Petlje/13.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main() {
-
+    int br1(0), br2(0);
+    for(int i = 10; i <= 99; i++) {
+        br1 = (i / 10) % 10;
+        br2 = i % 10;
+        if(br1 + br2 < 5) {
+            cout << i << '\n';
+        }
+    }
     return 0;
 }
 /*
