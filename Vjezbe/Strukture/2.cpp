4cba742806f88550cd51358429288322e644fcc0
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 17:01:28 +0200

    Vjezbe - Strukture - Zavrsen 2. zadatak

--- Vjezbe/Strukture/2.cpp
+++ Vjezbe/Strukture/2.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+struct tacka {
+    int x;
+    int y;
+} k;
 
+tacka unosKoordinata() {
+    cout << "Unesite koordinate x i y!\n";
+    cin >> k.x >> k.y;
+    return k;
+}
+
+tacka ispisKoordinata(tacka t1, tacka t2) {
+    cout << "Udaljenost tacaka je: " << sqrt(pow(t2.x - t1.x, 2) + pow(t2.y - t1.y,2));
+}
+
+int main() {
+    tacka t1 = unosKoordinata();
+    tacka t2 = unosKoordinata();
+
+    ispisKoordinata(t1, t2);
     return 0;
 }
 /*
