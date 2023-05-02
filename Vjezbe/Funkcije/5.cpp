fad84789b72d58c901eb84ccd1e202a156aa7c89
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 03:04:54 +0200

    Vjezbe - Funkcije - Zavrsen 5. zadatak

--- Vjezbe/Funkcije/5.cpp
+++ Vjezbe/Funkcije/5.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int IzracunajSumu(int N) {
+    int n, s(0);
+    for(int i = 1; i <= N; i++) {
+        cout << "Unesite " << i << ". broj:\n";
+        cin >> n;
+        s += n;
+    }
+    return s;
+}
 
+int main() {
+    int n;
+    cout << "Unesite broj:\n";
+    cin >> n;
+
+    cout << IzracunajSumu(n);
     return 0;
 }
 /*
