9d89a3133e253a445e2a1e19926cb11f1a10baca
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 03:12:23 +0200

    Vjezbe - Funkcije - Postavljen 7. zadatak

---
+++ Vjezbe/Funkcije/7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkciju koja vraca najmanji sestocifreni broj koji je djeljiv sa 3 a nije djeljiv sa 5.
+*/
