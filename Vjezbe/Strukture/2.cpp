0742da5faa1e6de270301675b286f8144440ed49
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 16:55:31 +0200

    Vjezbe - Strukture - Postavljen 2. zadatak

---
+++ Vjezbe/Strukture/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati strukturu pod nazivom tacka sa atributima koordinatim tackama x i y kreirati funkciju za unos koorditana i ispis njihove
+udaljenostis
+*/
