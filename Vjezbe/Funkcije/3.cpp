9dba291032a2e3c618304d7f3e03c4c549b3dc89
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 02:49:15 +0200

    Vjezbe - Funkcije - Zavrsen 3. zadatak

--- Vjezbe/Funkcije/3.cpp
+++ Vjezbe/Funkcije/3.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int suma(int a, int b) {
+    return a + b;
+}
 
+int main() {
+    int x, y;
+    cout << "Unesite dva broja:\n";
+    cin >> x >> y;
+
+    int z = suma(x, y);
+    cout << z;
     return 0;
 }
 /*
