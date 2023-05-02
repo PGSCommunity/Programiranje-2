95013c31d4835ab1ac00ceae9bd7c8e647747f3c
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 03:10:40 +0200

    Vjezbe - Funkcije - Zavrsen 6. zadatak

--- Vjezbe/Funkcije/6.cpp
+++ Vjezbe/Funkcije/6.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int brojBrojeva(int k, int m, int a) {
+    int br(0);
+    for(int i = k; i <= m; i++) {
+        if(i % 10 == a) br++;
+    }
+    return br;
+}
 
+int main() {
+    int k, m, a;
+    cout << "Unesite brojeve k i m:\n";
+    cin >> k >> m;
+    cout << "Unesite broj a:\n";
+    cin >> a;
+
+    cout << brojBrojeva(k, m, a);
     return 0;
 }
 /*
