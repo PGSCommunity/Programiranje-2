f39d08fa702bb6f1fb02a68e70473109c5e8740c
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 02:58:44 +0200

    Vjezbe - Funkcije - Postavljen 5. zadatak

---
+++ Vjezbe/Funkcije/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji ucitava broj N, nakon cega se poziva funkcija IzracunajSumu koja omogucava unos N brojeva i vraca 
+vrijednost njihove sume.
+*/
