68c906fe85bb482f665b0d6b2d1220af5cc4bcbb
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 02:51:46 +0200

    Vjezbe - Funkcije - Postavljen 4. zadatak

---
+++ Vjezbe/Funkcije/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program za simulaciju rada kalkulatora za 4 osnovne operacije. Program treba da omogući korisniku unos dvije vrijednosti
+i znaka za operaciju, a nakon toga se poziva adekvatna funkcija koja odgovara izboru korisnika na osnovu unesenog znaka. 
+*/
