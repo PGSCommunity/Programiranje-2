5b7672190a9f7563b19a60764f624b689db4bb11
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 03:06:27 +0200

    Vjezbe - Funkcije - Postavljen 6. zadatak

---
+++ Vjezbe/Funkcije/6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkciju koja vraca broj brojava iz intervala k - m cija je cifra jedinice jednaka broju a 
+*/
