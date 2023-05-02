d8ca9efd8c02080e7b420e8338e23653f71ac274
Danin Sadzak <daninsadzak@gmail.com>
2023-05-02 13:54:11 +0200

    Vjezbe - Strukture - Postavljen 1. zadatak

---
+++ Vjezbe/Strukture/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati strukturu pod nazivom datum sa atributima dan, mjesec i godina kreirati funkciju za unos i ispis datuma
+*/
