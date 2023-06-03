2b034b8e4079f60ab51c658ef7d59119d7ec9856
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 23:37:31 +0200

    Vjezbe - Strukture - Zavrsen 6. zadatak

--- Vjezbe/Strukture/6.cpp
+++ Vjezbe/Strukture/6.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct vrijeme {
+    int sec;
+};
 
+int main() {
+    vrijeme v[2];
+
+    for(int i = 0; i < 2; i++) {
+        cout << "Unesite " << i + 1 << ". sekundu!\n";
+        cin >> v[i].sec;
+        cout << "===================\n\n";
+    } 
+
+    for(int i = 0; i < 2; i++) {
+        int h = v[i].sec / 3600;
+        int m = v[i].sec % 3600 / 60;
+        cout << i + 1 << ". vrijeme u obliku hh/mm/sec:\n";
+        cout << h << "/" << m << "/" << v[i].sec << '\n';
+        cout << "====================\n\n";
+    }
     return 0;
 }
 /*
