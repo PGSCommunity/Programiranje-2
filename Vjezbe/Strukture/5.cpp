6ddbebffe3a7a0937b4c68ffef164c746566cd77
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 23:31:03 +0200

    Vjezbe - Strukture - Zavrsen 5. zadatak

--- Vjezbe/Strukture/5.cpp
+++ Vjezbe/Strukture/5.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+struct kvadrat {
+    float d;
+};
 
+int main() {
+    kvadrat k[6];
+
+    for(int i = 0; i < 6; i++) {
+        cout << "Unesite dijagonalu " << i + 1 << ". dijagonale!\n";
+        cin >> k[i].d;
+        cout << "====================\n\n";
+    }
+
+    for(int i = 0; i < 6; i++) {
+        float a = (k[i].d * sqrt(2)) / 2;
+        cout << "Povrsina " << i + 1 << ". dijagonale:\n";
+        cout << a * a << '\n';
+        cout << "Obim " << i + 1 << ". dijagonale:\n";
+        cout << 4 * a << '\n';
+        cout << "====================\n\n";
+    }
     return 0;
 }
 /*
