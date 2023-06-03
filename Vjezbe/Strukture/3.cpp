d2fc520097e2ac344a5c828a27da0e8750cbe9be
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 23:01:31 +0200

    Vjezbe - Strukture - Zavrsen 3. zadatak

--- Vjezbe/Strukture/3.cpp
+++ Vjezbe/Strukture/3.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct trougao {
+    int osnovica;
+    int visina;
+} t;
 
+trougao unos() {
+    cout << "Unesite osnovicu!\n";
+    cin >> t.osnovica;
+    cout << "Unesite visinu!\n";
+    cin >> t.visina;
+    return t;
+}
+
+trougao visina(trougao t1, trougao t2) {
+    float p1, p2;
+    p1 = (t.osnovica * t.visina) / 2;
+    p2 = (t.osnovica * t.visina) / 2;
+    if(p1 > p2) cout << "Veci je: " << p1;
+    else cout << "Veci je: " << p2;
+}
+
+int main() {
+    trougao t1, t2;
+    t1 = unos();
+    t2 = unos();
+    visina(t1, t2);
     return 0;
 }
 /*
