b8fa84501ab592c0127d2f138170624784c1cf75
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 23:04:15 +0200

    Vjezbe - Strukture - Postavljen 4. zadatak

---
+++ Vjezbe/Strukture/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu 'zaposlenik' ( broj, ime, prezime, zanimanje, plata ), zatim napisati program koji sadrži niz od 5 
+zaposlenika a koji omogućava unos pa ispis podataka definiranih strukturom za svakog od 5 zaposlenika.
+*/
