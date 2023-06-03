cf26a9103b9fe423867cfe839e4f9ecabb19c5af
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 23:24:21 +0200

    Vjezbe - Strukture - Postavljen 5. zadatak

---
+++ Vjezbe/Strukture/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu ‚kvadrat' ( dijagonala kvadrata ), zatim napisati program koji za niz od šest kvadrata ispisuje površinu i 
+obim za svaki, nakon učitane vrijednosti dijagonale.
+*/
