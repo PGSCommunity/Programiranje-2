32f0ca9429d1e556807368535d61fb35c211e293
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 23:12:23 +0200

    Vjezbe - Strukture - Zavrsen 4. zadatak

--- Vjezbe/Strukture/4.cpp
+++ Vjezbe/Strukture/4.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct zaposlenik {
+    int broj;
+    string ime;
+    string prezime;
+    string zanimanje;
+    float plata;
+};
 
+int main() {
+    zaposlenik z[5];
+
+    for(int i = 0; i < 5; i++) {
+        cout << "Unesite broj " << i + 1 << ". zaposlenika!\n";
+        cin >> z[i].broj;
+        cout << "Unesite ime " << i + 1 << ". zaposlenika!\n";
+        cin >> z[i].ime;
+        cout << "Unesite prezime " << i + 1 << ". zaposlenika!\n";
+        cin >> z[i].prezime;
+        cout << "Unesite zanimanje " << i + 1 << ". zaposlenika!\n";
+        cin >> z[i].zanimanje;
+        cout << "Unesite platu " << i + 1 << ". zaposlenika!\n";
+        cin >> z[i].plata;
+        cout << "====================\n\n"; 
+    }
+
+    for(int i = 0; i < 5; i++) {
+        cout << "ISPIS PODATAKA " << i + 1 << ". ZAPOSLENIKA:\n\n";
+        cout << "Broj: " << z[i].broj << '\n';
+        cout << "Ime: " << z[i].ime << '\n';
+        cout << "Prezime: " << z[i].prezime << '\n';
+        cout << "Zanimanje: " << z[i].zanimanje << '\n';
+        cout << "Plata: " << z[i].plata << '\n';
+        cout << "====================\n\n"; 
+    }
     return 0;
 }
 /*
