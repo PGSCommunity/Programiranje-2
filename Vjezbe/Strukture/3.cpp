18e86a3a33e526be82916395a51e506b7c4c7159
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 22:55:31 +0200

    Vjezbe - Strukture - Postavljen 3. zadatak

---
+++ Vjezbe/Strukture/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati strukturu trougao sa atributima osnovica i visina. Kreirati dvije funkcije, unos i povrsina. Program racuna povrsine za
+dva trougla i ispisuje povrsinu vece.
+*/
