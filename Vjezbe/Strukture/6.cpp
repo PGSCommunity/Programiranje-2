ae13fdd5faac0b1ba2bd6991be0998e7a2136554
Danin Sadzak <daninsadzak@gmail.com>
2023-06-03 23:32:01 +0200

    Vjezbe - Strukture - Postavljen 6. zadatak

---
+++ Vjezbe/Strukture/6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu ‚vrijeme' ( sec ), zatim napisati program koji sadrži niz dimenzije 2 za strukturni tip podatka 'vrijeme‘‚ 
+a koji omogućava ispis vremena u obliku hh/mm/sec za uneseno vrijeme u sekundama.
+*/
