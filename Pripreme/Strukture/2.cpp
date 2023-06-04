38251216459074521539027ba694fde9c63fc482
Danin Sadzak <daninsadzak@gmail.com>
2023-06-04 13:44:38 +0200

    Pripreme - Strukture - Zavrsen 2. zadatak

--- Pripreme/Strukture/2.cpp
+++ Pripreme/Strukture/2.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 using namespace std;
 
+struct krug {
+    float r;
+} k;
+
+const float PI = 3.14;
+
+krug unos() {
+    cout << "Unesite poluprecnik kruga!\n";
+    cin >> k.r;
+    return k;
+}
+
+float ispisObima(krug k1) {
+    return 2 * k1.r * PI;
+}
+
+float dvostruka(krug k1) {
+    return 2 * (k1.r * k1.r * PI); 
+}
+ 
 int main() {
-    
+    krug k1 = unos();
+    cout << "Obim kruga " << ispisObima(k1) << '\n';
+    cout << "Dvostruka vrijednost povrsine kruga: " << dvostruka(k1);
     return 0;
 }
 /*
