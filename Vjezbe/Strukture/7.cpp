8408e33af3133f46ccc5890a8e4d1f3adf8dc511
Danin Sadzak <daninsadzak@gmail.com>
2023-06-04 13:19:03 +0200

    Vjezbe - Strukture - Zavrsen 7. zadatak

--- Vjezbe/Strukture/7.cpp
+++ Vjezbe/Strukture/7.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct ucenik {
+    string ime;
+    string prezime;
+    int ocjena;
+} u;
 
+void ispisPodataka(ucenik u) {
+    cout << "ISPIS PODATAKA UCENIKA:\n";
+    cout << "Ime: " << u.ime << '\n';
+    cout << "Prezime: " << u.prezime << '\n';
+    cout << "Ocjena: " << u.ocjena << '\n';
+}
+
+int main() {
+    cout << "Unesite ime ucenika!\n";
+    cin >> u.ime;
+    cout << "Unestie prezime ucenika!\n";
+    cin >> u.prezime;
+    cout << "Unesite ocjenu ucenika!\n";
+    cin >> u.ocjena;
+    ispisPodataka(u);
     return 0;
 }
 /*
