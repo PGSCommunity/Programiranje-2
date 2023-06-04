2692de84904784bbecfe301ffd69bdfaed7d002a
Danin Sadzak <daninsadzak@gmail.com>
2023-06-04 13:34:02 +0200

    Prirpreme - Srukture - Postavljen 2. zadatak

---
+++ Pripreme/Strukture/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
+/*
+Definirati strukturu Krug. Zatim napisati funkciju koja omogućava unos vrijednosti parametara definiranih strukturom, 
+funkciju koja vraća obim kruga te funkciju koja ispisuje dvostruku vrijednost površine kruga. 
+*/
