d6903b08acdd0a548009ec38897d86d32f0a97cc
Danin Sadzak <daninsadzak@gmail.com>
2023-06-04 13:23:17 +0200

    Pripreme - Strukture - Postavljen 1. zadatak

---
+++ Pripreme/Strukture/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    return 0;
+}
+/*
+Pomoću strukture definisati i ispisati ime, prezime učenika te ocjene iz Matematike, Fizike i Programiranja. Ispisati ime, 
+prezime te prosječnu ocjenu iz navedenih predmeta.
+*/
