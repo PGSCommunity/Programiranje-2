9cf0f86ac5a9a8303717abb10ea07dbe0e1945bf
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 15:36:25 +0200

    Pripreme - Strukture - Postavljen 5. zadatak

--- Pripreme/Strukture/5.cpp
+++ Pripreme/Strukture/5.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+
+
 int main() {
 
     return 0;
 }
 /*
-Pomocu strukture definisati i upisati ime, prezime te opci uspjeh za 2 uèenika. Ispisati ime i prezime ucenika koji ima bolji 
+Pomocu strukture definisati i upisati ime, prezime te opci uspjeh za 2 ucenika. Ispisati ime i prezime ucenika koji ima bolji 
 uspjeh!
 */
