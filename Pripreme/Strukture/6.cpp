1710baebea1fd0bdbfa6466200681ae36776511e
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 15:51:08 +0200

    Pripreme - Strukure - Zavrsen 6. zadatak

--- Pripreme/Strukture/6.cpp
+++ Pripreme/Strukture/6.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct kompleksnibroj {
+    float realniDio;
+    float imaginarniDio;
+} k;
 
+kompleksnibroj unos() {
+    cout << "Unesite realni broj:\n";
+    cin >> k.realniDio;
+    cout << "Unesite imaginarni broj:\n";
+    cin >> k.imaginarniDio;
+    return k;
+}
+
+kompleksnibroj zbirBrojeva(kompleksnibroj k1, kompleksnibroj k2) {
+    kompleksnibroj zbir;
+    zbir.realniDio = k1.realniDio + k2.realniDio;
+    zbir.imaginarniDio = k1.imaginarniDio + k2.imaginarniDio;
+    return zbir;
+}
+
+int main() {
+    kompleksnibroj k1(unos()), k2(unos());
+    kompleksnibroj zbir(zbirBrojeva(k1, k2));
+
+    cout << "Zbir realnog dijela brojeva: " << zbir.realniDio << '\n';
+    cout << "Zbir imaginarnog dijela brojeva: " << zbir.imaginarniDio; 
     return 0;
 }
 /*
