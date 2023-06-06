e359a6c78a172f2965fb92a29ed3d5e1c01ce55f
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 14:10:56 +0200

    Pripreme - Strukture - Zavrsen 3, zadatak

--- Pripreme/Strukture/3.cpp
+++ Pripreme/Strukture/3.cpp
@@ -1,8 +1,65 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct ucenik {
+    string ime;
+    string prezime;
+    int ocjena_testa;
+};
 
+int main() {
+    int x;
+    cout << "Koliko ucenika?\n";
+    cin >> x;
+    ucenik u[x];
+
+    int br5(0), br4(0), br3(0), br2(0), br1(0);
+    float suma(0), br(0);
+    for(int i = 0; i < x; i++) {
+        cout << "Unesite ime " << i + 1 << ". ucenika!\n";
+        cin >> u[i].ime;
+        cout << "Unesite prezime " << i + 1 << ". ucenika!\n";
+        cin >> u[i].prezime;
+        cout << "Unesite ocjenu sa testa " << i + 1 << ". ucenika!\n";
+        unos:cin >> u[i].ocjena_testa;
+        switch(u[i].ocjena_testa) {
+            case 1:
+                br1++;
+                break;
+            case 2:
+                br2++;
+                break;
+            case 3:
+                br3++;
+                break;
+            case 4:
+                br4++;
+                break;
+            case 5:
+                br5++;
+                break;
+            default:
+                cout << "Pogresan unos! (1-5):\n";
+                goto unos;
+        }
+        suma += u[i].ocjena_testa;
+        cout << "====================\n\n";
+    }
+    br = br5 + br4 + br3 + br2 + br1;
+
+    cout << "ISPIS UKUPNIH OCJENA:\n";
+    cout << "Ukupan broj odlicnih ocjena: " << br5 << '\n';
+    cout << "Ukupan broj vrlodobrih ocjena: " << br4 << '\n';
+    cout << "Ukupan broj dobrih ocjena: " << br3 << '\n';
+    cout << "Ukupan broj dovoljnih ocjena: " << br2 << '\n';
+    cout << "Ukupan broj nedovoljnih ocjena: " << br1 << "\n\n"; 
+
+    cout << "Prosjecna ocjena na testu: " << suma / br << "\n\n";
+
+    cout << "Imena i prezimena ucenika koji su dobili odlicnu ocjenu:\n";
+    for(int i = 0; i < x; i++) {
+        if(u[i].ocjena_testa == 5) cout << u[i].ime << " " << u[i].prezime << '\n';
+    }
     return 0;
 }
 /*
