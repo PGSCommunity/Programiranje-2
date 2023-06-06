e3a716593ecacd3c8606cec739f475972dcc28bf
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 16:10:40 +0200

    Pripreme - Strukture - Zavrsen 7. zadatak

--- Pripreme/Strukture/7.cpp
+++ Pripreme/Strukture/7.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct zeljeznicar {
+    string prezime;
+    int broj;
+    string pozicija;
+    int brojGolova;
+};
 
+int main() {
+    zeljeznicar z[3];
+
+    float suma(0);
+    for(int i = 0; i < 3; i++) {
+        cout << "Unesite prezime " << i + 1 << ". igraca!\n";
+        cin >> z[i].prezime;
+        cout << "Unesite broj " << i + 1 << ". igraca!\n";
+        cin >> z[i].broj;
+        cout << "Unesite poziciju " << i + 1 << ". igraca!\n";
+        cin >> z[i].pozicija;
+        cout << "Unesite broj golova " << i + 1 << ". igraca!\n";
+        cin >> z[i].brojGolova;
+        suma += z[i].brojGolova;
+        cout << "====================\n\n";
+    }
+
+    cout << "Prezimena igraca sa neparnim brojem:\n";
+    for(int i = 0; i < 3; i++) {
+        if(z[i].broj % 2 != 0) cout << z[i].prezime << '\n';
+    }
+    cout << endl;
+
+    int maximum(0);
+    int index(0);
+    for(int i = 0; i < 3; i++) {
+        if(maximum < z[i].brojGolova) {
+            maximum = z[i].brojGolova;
+            index = i;
+        }
+    } 
+    cout << "Najefikasniji igrac je " << z[index].prezime << " sa " << z[index].brojGolova << " postignutih golova\n\n";
+
+    cout << "Prosjecan broj golova u klubu je: " << suma / 3 << "\n\n";
+
+    for(int i = 0; i < 3; i++) {
+        if(z[i].pozicija == "golman") {
+            cout << "Prezime golmana: " << z[i].prezime << '\n';
+            cout << "Broj golmana: " << z[i].broj << '\n';
+        }
+    }
     return 0;
 }
 /*
