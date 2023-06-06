28f97ff2200e1bf3411af5ff6a05753929e591c2
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 15:52:57 +0200

    Pripreme - Strukture - Postavljen 7. zadatak

---
+++ Pripreme/Strukture/7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu klub npr.'Željeznicar' (prezime(igraca),  broj (dres), pozicija,  broj golova), zatim napisati program koji 
+sadrži niz od 11 igraca, omogucava unos podataka definiranih strukturom te ispis:
+a) prezimena igraca sa neparnim brojem
+b) najefikasnijeg igraca (broj postignutih golova)
+c) prosjecan broj golova u klubu
+d) sve podatke o igracu na poziciji golman.
+*/
