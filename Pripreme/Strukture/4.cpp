60bd943a584207f6d54194f68f01d1e54568de86
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 14:29:25 +0200

    Pripreme - Strukture - Zavrsen 4. zadatak

--- Pripreme/Strukture/4.cpp
+++ Pripreme/Strukture/4.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct Stol {
+    float duzina;
+    float sirina;
+    float visina;
+};
 
+float povrsinaStola(Stol s[], int n) {
+    float maximum(0);
+    float p(0);
+    for(int i = 0; i < n; i++) {
+        p = s[i].duzina * s[i].sirina;
+        if(maximum < p) {
+            maximum = p;
+        }
+    }
+    return maximum;
+}
+
+int main() {
+    int n;
+    cout << "Koliko stolova?\n";
+    cin >> n;
+    Stol s[n];
+
+    for(int i = 0; i < n; i++) {
+        cout << "Unesite duzinu " << i + 1 << ". stola!\n";
+        cin >> s[i].duzina;
+        cout << "Unesite sirinu " << i + 1 << ". stola!\n";
+        cin >> s[i].sirina;
+        cout << "Unesite visinu " << i + 1 << ". stola!\n";
+        cin >> s[i].visina;
+        cout << "====================\n\n";
+    }
+
+    cout << "Najveca pisacka povrsina iznosi: " << povrsinaStola(s, n) << "cm.\n";
     return 0;
 }
 /*
