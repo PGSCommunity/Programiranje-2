f8ae5cfe11ace58f18d3d17bd2ce1725a026dd3e
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 15:44:01 +0200

    Prirpeme - Strukture - Postavljen 6. zadatak

---
+++ Pripreme/Strukture/6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Pomocu strukture definirati realni i imaginarni dio kompleksnog broja. Zatim ucitati dva kompleksna broja i izracunati njihov zbir
+*/
