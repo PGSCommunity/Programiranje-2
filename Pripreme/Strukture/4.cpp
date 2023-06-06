a72a294282e51b963234024729f92078b45928f5
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 14:12:38 +0200

    Pripreme - Strukture - Postavljen 4. zadatak

---
+++ Pripreme/Strukture/4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu stol. Stol je određen sa svojom dužinom, širinom i visinom. Zatim napisati program koja za niz stolova
+vraća onaj stol (njegovu površinu) koji imaj najveću pisaću površinu.
+*/
