55ee2eefce05944fd1bec9081fd860564175d824
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 13:40:19 +0200

    Pripreme - Strukture - Postavljen 3. zadatak

---
+++ Pripreme/Strukture/3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definisati strukturu Ucenik ( ime, prezime, ocjena_testa). Napisati program koji za x ucenika ispisuje broj ucenika koji su
+dobili odličnu, vrlodobru, dobru, dovoljnu i u konačnici nedovoljnu ocjenu na testu. Zatim ispisati prosječnu ocjenu na testu te
+imena i prezimena učenika koji su dobili odličnu ocjenu.
+*/
