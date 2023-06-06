ec6c1d4cadc98bb4f58854f93a3bd886f7bfbc2e
Danin Sadzak <daninsadzak@gmail.com>
2023-06-06 15:41:43 +0200

    Pripreme - Strukture - Zavrsen 5. zadatak

--- Pripreme/Strukture/5.cpp
+++ Pripreme/Strukture/5.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 using namespace std;
 
+struct ucenik {
+    string ime;
+    string prezime;
+    float prosjek;
+} u;
 
+ucenik unos() {
+    cout << "Unesite ime ucenika!\n";
+    cin >> u.ime;
+    cout << "Unesite prezime ucenika!\n";
+    cin >> u.prezime;
+    cout << "Unesite prosjek ucenika!\n";
+    cin >> u.prosjek;
+    return u;
+}
+
+void boljiProsjek(ucenik u1, ucenik u2) {
+    if(u1.prosjek > u2.prosjek) cout << "Veci prosjek ima: " << u1.ime << " " << u1.prezime;
+    else cout << "Veci prosjek ima: " << u2.ime << " " << u2.prezime;
+}
 
 int main() {
-
+    ucenik u1(unos()), u2(unos());
+    boljiProsjek(u1, u2);
     return 0;
 }
 /*
