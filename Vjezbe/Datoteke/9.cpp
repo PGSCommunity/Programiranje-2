1936e562ae761054b9caeda06c56d6a4041002f6
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 11:18:32 +0200

    Vjezbe - Datoteke - Zavrsen 9. zadatak

--- Vjezbe/Datoteke/9.cpp
+++ Vjezbe/Datoteke/9.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int main() {
-
+    ofstream prva("prva.txt");
+    fstream druga("druga.txt", ios::out);
+    if(!prva) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int niz[10];
+        int suma(0), proizvod(1);
+        for(int i = 0; i < 10; i++) {
+            cout << "Unesite " << i + 1 << ". broj!\n";
+            cin >> niz[i];
+            prva << niz[i] << " ";
+            if(niz[i] >= 1 && niz[i] <= 9) proizvod *= niz[i];
+            if(niz[i] >= 10 && niz[i] <= 99) suma += niz[i];
+        }
+        druga << proizvod << '\n';
+        druga << suma << '\n';
+        druga.close();
+        druga.open("druga.txt", ios::in);
+        int broj;
+        while(druga >> broj) cout << broj << '\n';
+    }
     return 0;
 }
 /*
