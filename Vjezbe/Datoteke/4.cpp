9bc3d78b0419b96563a907795c2b4385f39d5f2d
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:26:59 +0200

    Vjezbe - Datoteke - Zavrsen 4. zadatak

--- Vjezbe/Datoteke/4.cpp
+++ Vjezbe/Datoteke/4.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
+    string ime;
+    cout << "Unesite ime datoteke!\n";
+    getline(cin, ime);
 
+    ofstream ucitavanje(ime.c_str());
+    if(!ucitavanje) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int niz[4];
+        for(int i = 0; i < 4; i++) {
+            cout << "Unesite " << i + 1 << ". broj!\n";
+            cin >> niz[i];
+            ucitavanje << niz[i] << '\n';
+        }
+    }
     return 0;
 }
 /*
