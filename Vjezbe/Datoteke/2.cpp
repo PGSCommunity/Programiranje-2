32eb4902f4cbc3b123464ef93ac991a5d68867a4
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:07:38 +0200

    Vjezbe - Datoteke - Postavljen 2. zadatak

---
+++ Vjezbe/Datoteke/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji računa aritmetičku sredinu 7 parnih brojeva koji se nalaze u datoteci „aritmeticka.txt„.Koristeći 
+promjenljivu izlaznog toka datoteke „aritmeticka.txt", iz datoteke „dvocifreni.txt" ( u kojoj se nalazi 10 različitih brojeva) 
+program treba da izdvoji i ispiše sumu dvocifrenih brojeva. Za obje datoteke provjeriti eventualne razloge neispravnosti 
+učitavanja vrijednosti.
+*/
