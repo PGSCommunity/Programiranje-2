5da3ce755f9576f9a0fbe730561755e4ef84234a
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 11:09:30 +0200

    Vjezbe - Datoteke - Zavrsen 8. zadatak

--- Vjezbe/Datoteke/8.cpp
+++ Vjezbe/Datoteke/8.cpp
@@ -4,7 +4,14 @@
 using namespace std;
 
 int main() {
-
+    ifstream recenica("recenica.txt");
+    if(!recenica) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        string str;
+        getline(recenica, str);
+        for(int i = str.size() - 1; i >= 0; i--) cout << str[i];
+    }
     return 0;
 }
 /*
