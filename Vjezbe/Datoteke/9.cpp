462fe439d5d20afbb5ef7bca4c5a397a0adb8bfa
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 11:10:45 +0200

    Vjezbe - Datoteke - Postavljen 9. zadatak

---
+++ Vjezbe/Datoteke/9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji korisniku omogucava upis 10 brojeva u datoteku "prva.txt".
+Zatim se u datoteku "druga.txt" u prvi red upisuje proizvod jednocifrenih a u drugi red suma dvocifrenih brojeva.
+Na kraju se podaci iz datoteke druga.txt ispisuju na ekran
+*/
