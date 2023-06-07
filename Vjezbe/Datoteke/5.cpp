9dee1205ae06285f8d879ff099683dd0b999a083
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:43:22 +0200

    Vjezbe - Datoteke - Zavrsen 5. zadatak

--- Vjezbe/Datoteke/5.cpp
+++ Vjezbe/Datoteke/5.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 using namespace std;
 
 int main() {
+    fstream upis("petbrojeva.txt", ios::out);
 
+    if(!upis) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int niz[5];
+        for(int i = 0; i < 5; i++) {
+            cout << "Unesite " << i + 1 << ". broj!\n";
+            cin >> niz[i];
+            upis << "Upis " << i + 1 << ". broja: " << niz[i] << '\n';
+            upis << "Upis kvadrata " << i + 1 << ". broja: " << pow(niz[i], 2) << '\n';
+        }
+    }
     return 0;
 }
 /*
