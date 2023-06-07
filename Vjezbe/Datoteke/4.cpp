3a5bdaaeb290e263b995b0f3281d095c7f3ed08f
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:23:44 +0200

    Vjezbe - Datoteke - Postavljen 4. zadatak

---
+++ Vjezbe/Datoteke/4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji kreira datoteku korisnički definiranog naziva i omogućava učitavanje 4 broja u tu datoteku
+*/
