3f5c9cc9b35fe1d53e072c012d3bcc796d9bb5ba
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 09:56:54 +0200

    Vjezbe - Datoteke - Postavljen 1. zadatak

---
+++ Vjezbe/Datoteke/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Izračunati proizvod x parnirnih brojeva niza iz raspona m - n. Kreirati datoteku „proizvod.txt" (ispisati poruku ukoliko je 
+kreiranje datoteke neuspješno), pa u datoteku upisati broj članova niza, članove niza, interval m - n i traženi rezultat.
+*/
