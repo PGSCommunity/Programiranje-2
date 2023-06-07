46ab2566d1c7a2f774efc7d53c7721b4a3cf1444
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:20:32 +0200

    Vjezbe - Datoteke - Postavljen 3. zadatak

---
+++ Vjezbe/Datoteke/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Kreirati datoteku „brojevi.txt“ u koju je potrebno sačuvati brojeve iz intervala 1 – 10.
+Napisati program koji učitava te brojeve u jednom redu razdvojene razmakom
+*/
