9bdd7b5923e9baac05266c08b3b720fbdd3c0b96
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:45:58 +0200

    Vjezbe - Datoteke - Postavljen 6. zadatak

---
+++ Vjezbe/Datoteke/6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji pomoću klase „fstream“ i objekta iz datoteke cita podatke
+*/
