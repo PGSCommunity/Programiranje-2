ec362a360867eaaeda68068a102388e2eb120545
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:18:35 +0200

    Vjezbe - Datoteke - Zavrsen 2. zadatak

--- Vjezbe/Datoteke/2.cpp
+++ Vjezbe/Datoteke/2.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
+    ifstream aritmeticka("aritmeticka.txt");
+    if(!aritmeticka) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int niz1[7];
+        float suma1(0);
+        for(int i = 0; i < 7; i++) {
+            aritmeticka >> niz1[i];
+            suma1 += niz1[i];
+        }
+        cout << "Aritmeticka sredina brojeva iz datoteke: " << suma1 / 7 << '\n';
+    }
 
+    ifstream dvocifreni("dvocifreni.txt");
+    if(!dvocifreni) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int niz2[10], suma2(0);
+        for(int i = 0; i < 10; i++) {
+            dvocifreni >> niz2[i];
+            if(niz2[i] >= 10 && niz2[i] <= 99) suma2 += niz2[i];
+        }
+        cout << "Suma dvocifrenih brojeva iz datoteke: " << suma2;
+    }
     return 0;
 }
 /*
