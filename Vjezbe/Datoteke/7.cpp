a2ae902b70f42d6879e4e8a2943c461d175d2c5f
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:59:36 +0200

    Vjezbe - Datoteke - Zavrsen 7. zadatak

--- Vjezbe/Datoteke/7.cpp
+++ Vjezbe/Datoteke/7.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 
 int main() {
-
+    fstream unos("upisbrojeva.txt", ios::out);
+    fstream ispis("ispisbrojeva.txt", ios::out);
+    if(!unos || !ispis) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int niz[5];
+        for(int i = 0; i < 5; i++) {
+            cout << "Unesite " << i + 1 << ". broj!\n";
+            cin >> niz[i];
+            unos << niz[i] << '\n';
+            if(niz[i] % 2 == 0) ispis << niz[i] << '\n';
+        }
+        ispis.close();
+        ispis.open("ispisbrojeva.txt", ios::in);
+        int broj;
+        while(ispis >> broj) cout << broj << '\n';
+    }
     return 0;
 }
 /*
