492703ab071218af0d25eed72fa3f76371d78bf6
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:49:56 +0200

    Vjezbe - Datoteke - Zavrsen 6. zadatak

--- Vjezbe/Datoteke/6.cpp
+++ Vjezbe/Datoteke/6.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
-
+    fstream broj("mojibrojevi.txt", ios::in);
+    if(!broj) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        string str;
+        while(getline(broj, str)) cout << str << '\n';
+    }
     return 0;
 }
 /*
