ebfe3db56de5e2e24e026db091b05c1400db3575
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:06:20 +0200

    Vjezbe - Datoteke - Zavrsen 1. zadatak

--- Vjezbe/Datoteke/1.cpp
+++ Vjezbe/Datoteke/1.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
+    ofstream proizvod("proizvod.txt");
+    int m, n;
+    cout << "Od broja?\n";
+    cin >> m;
+    cout << "Do broja?\n";
+    cin >> n;
 
+    if(!proizvod) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int p(1), br(0);
+        for(int i = m; i <= n; i++) {
+            if(i % 2 == 0) p *= i;
+            br++;
+        }
+        proizvod << "Proizvod parnih brojeva u intervalu: " << p << '\n';
+        proizvod << "Broj clanova niza: " << br << '\n';
+        proizvod << "Clanovi niza u intervalu:\n";
+        for(int i = m; i <= n; i++) {
+            proizvod << i << " ";
+        }
+    }
     return 0;
 }
 /*
