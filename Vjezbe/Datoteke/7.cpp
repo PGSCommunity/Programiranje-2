bc511902f158b293a1cbf19e47387dccf8f629a1
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:52:07 +0200

    Vjezbe - Datoteke - Postavljen 7. zadatak

---
+++ Vjezbe/Datoteke/7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji pomoću klase „fstream“ i dva razlicita objekta kreira dvije datoteke. U jednu se vrši upis 5 brojeva a u 
+drugu kao i na ekran izdvajaju parni brojevi.
+*/
