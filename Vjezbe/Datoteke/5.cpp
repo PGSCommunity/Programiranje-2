67f539f7b7c4a5e3e66c0c92dffe8cca25788e57
Danin Sadzak <daninsadzak@gmail.com>
2023-06-07 10:38:24 +0200

    Vjezbe - Datoteke - Postavljen 5. zadatak

---
+++ Vjezbe/Datoteke/5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji pomoću klase „fstream“ i objekta kreira datoteku u koju se upisuje 5 brojeva i kvadrata tih brojeva
+*/
