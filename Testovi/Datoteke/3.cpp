a6fb95bfae78e13e085c4abd9f03f432556f20f3
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 23:21:30 +0200

    Testovi - Datoteke - Postavljen 3. zadatak

---
+++ Testovi/Datoteke/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+U fajlu studenti.txt nalazi se spisak studenata i osvojenih poena na kolokvijama (svaki student je u posebnom redu a zapisan je u
+slijedecem obliku: ime, prezime, kolokvij1, kolokvij2). Svaki kolokvij nosi 50 poena. Student je polozio ispit ako na svakom 
+kolokviju ima vise od 24, a u zbiru vise od 55 poena. Program treba da u fajl rezultati.txt upise one studente koji su polozili 
+ispit.
+*/
