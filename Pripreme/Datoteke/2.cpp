c6193b98c1e46c4a040ddaa8b96457444e2e35f7
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 19:38:04 +0200

    Pripreme - Datoteke - Postavljen 2. zadatak

---
+++ Pripreme/Datoteke/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji u datoteku „Proracun.txt“ upisuje brojeve n, m i aritmetičku sredinu brojeva iz intervala [n, m] djeljivih
+sa 5. 
+*/
