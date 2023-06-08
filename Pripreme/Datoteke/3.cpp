4ba81c838b4c19472d2ca1425aa309d663840635
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 19:58:07 +0200

    Pripreme - Datoteke - Postavljen 3. zadatak

---
+++ Pripreme/Datoteke/3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji brojeve datoteke „NizBrojeva.txt“ učitava u programski niz. Potom se iz niza prvo ispisuju negativni pa 
+pozitivni brojevi.
+*/
