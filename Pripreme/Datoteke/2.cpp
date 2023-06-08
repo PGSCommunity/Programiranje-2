73860b7e07dcc3f4a24fae98c69411fde0eb38a0
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 19:43:34 +0200

    Pripreme - Datoteke - Zavrsen 2. zadatak

--- Pripreme/Datoteke/2.cpp
+++ Pripreme/Datoteke/2.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 
 int main() {
-
+    ofstream proracun("Proracun.txt");
+    if(!proracun) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int n, m;
+        cout << "Od broja?\n";
+        cin >> n;
+        cout << "Do broja?\n";
+        cin >> m;
+        
+        proracun << "Brojevi iz intervala " << n << " do " << m << ":\n";
+        int suma(0), brojac(0); 
+        for(int i = n; i <= m; i++) {
+            proracun << i << '\n';
+            if(i % 5 == 0) {
+                suma += i;
+                brojac++;
+            }
+        }
+        proracun << "Aritmeticka sredina iz intervala djeljivih sa 5: " << suma / brojac;
+    }
     return 0;
 }
 /*
