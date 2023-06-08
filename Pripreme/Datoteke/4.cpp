70bfe852dd39be11382a70a0e7fad1834ec028db
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 20:07:46 +0200

    Pripreme - Datoteke - Postavljen 4. zadatak

---
+++ Pripreme/Datoteke/4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji omogućava stvaranje datoteke „Ucenici.txt“ i upis imena učenika te  ocjene iz predmeta Programiranje za 7
+učenika. Potom se u program pa datoteku „Prosjek.txt“ upisuje prosječna ocjena (aritmetička sredina) iz predmeta Programiranje 
+za navedene učenike. U prosjek se ne uračunavaju nedovoljne ocjene.
+*/
