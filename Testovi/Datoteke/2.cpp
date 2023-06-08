60002dd46bed8bd0bfa7055d594411b0044d635f
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 20:36:13 +0200

    Testovi - Datoteke - Postavljen 2. zadatak

---
+++ Testovi/Datoteke/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji za unesenu matricu dimenzije nxn u datoteku cije ime odreduje korisnik upisuje proizvod parnih elemenata na
+glavnoj dijagolani i elemente posljednje kolone matrice.
+*/
