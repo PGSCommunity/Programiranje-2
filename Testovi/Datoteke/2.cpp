cad40666f7d82b6832f2af5508e950e064369117
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 20:45:42 +0200

    Testovi - Datoteke - Zavrsen 2. zadatak

--- Testovi/Datoteke/2.cpp
+++ Testovi/Datoteke/2.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
-
+    string str;
+    cout << "Unesite ime datoteke!\n";
+    cin >> str;
+    ofstream matrica(str.c_str());
+    if(!matrica) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int n;
+        cout << "Unesite broj n!\n";
+        cin >> n;
+        int mat[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                cout << "Unesite matricu[" << i << "][" << j << "]:\n";
+                cin >> mat[i][j];
+            }
+        }
+        int p(1);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) {
+                    p *= mat[i][j];
+                }
+            }
+        }
+        matrica << "Proizvod elemenata na glavnoj dijagonali: " << p << '\n';
+        matrica << "Elementi zadnje kolone u matrici:\n";
+        for(int i = 0; i < n; i++) {
+            matrica << mat[i][n - 1] << '\n';
+        }
+    }
     return 0;
 }
 /*
