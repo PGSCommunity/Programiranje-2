ddfb5a3f2102d50c3d4f32fe5d9e135df5e2f57a
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 19:36:21 +0200

    Pripreme - Datoteke - Zavrsen 1. zadatak

--- Pripreme/Datoteke/1.cpp
+++ Pripreme/Datoteke/1.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
+    ifstream tekst("tekst.txt");
+    ofstream slovoa("slovo_a.txt");
 
+    if(!tekst || !slovoa) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        string str;
+        getline(tekst, str);
+
+        int br(0);
+        for(int i = 0; i < str.size(); i++) {
+            if(str[i] == 'a') br++; 
+        }
+
+        slovoa << "Broj ponavljanja slova a iz datoteke: " << br;
+    }
     return 0;
 }
 /*
