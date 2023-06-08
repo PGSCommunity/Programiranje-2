33a0809081f9d935d53f6f9faa0e4d1d6f31a640
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 19:29:21 +0200

    Pripreme - Datoteke - Postavljen 1. zadatak

---
+++ Pripreme/Datoteke/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji u izlaznu datoteku ispisuje broj pojavljivanja slova 'a' u tekstu koji je  učitan iz ulazne datoteke.
+*/
