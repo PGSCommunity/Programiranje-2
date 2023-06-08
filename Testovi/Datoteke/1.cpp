50eefadd3270a56d77e891978a6a769a1d86fd4b
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 20:33:39 +0200

    Testovi - Datoteke - Zavrsen 1. zadatak

--- Testovi/Datoteke/1.cpp
+++ Testovi/Datoteke/1.cpp
@@ -3,7 +3,18 @@
 using namespace std;
 
 int main() {
-
+    ifstream brojevi("realnibrojevi.txt");
+    if(!brojevi) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        int broj, x;
+        cout << "Unesite broj x:\n";
+        cin >> x;
+        cout << "Brojevi iz datoteke pomnozeni sa brojem x:\n";
+        while(brojevi >> broj) {
+            cout << broj * x << '\n';
+        }
+    }
     return 0;
 }
 /*
