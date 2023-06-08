432095798f2719558a92e590dc166d748bfb4920
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 20:16:24 +0200

    Pripreme - Datoteke - Zavrsen 4. zadatak

--- Pripreme/Datoteke/4.cpp
+++ Pripreme/Datoteke/4.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 int main() {
-
+    ofstream ucenici("Ucenici.txt");
+    ofstream prosjek("Prosjek.txt");
+    if(!ucenici || !prosjek) {
+        cout << "Ulaz u datoteku nije uspjelo!\n";
+    } else {
+        string ime;
+        int ocjena;
+        float suma(0), brojac(0);
+        for(int i = 0; i < 7; i++) {
+            cout << "Unesite ime " << i + 1 << ". ucenika!\n";
+            ucenici << "Upis podataka " << i + 1 << ". ucenika:\n";
+            cin.ignore();
+            getline(cin, ime);
+            ucenici << "Ime: " << ime << '\n';
+            cout << "Unesite ocjenu iz programiranja " << i + 1 << ". ucenika!\n";
+            cin >> ocjena;
+            ucenici << "Ocjena iz programiranja: " << ocjena << "\n\n";
+            if(ocjena > 1) {
+                suma += ocjena;
+                brojac++;
+            }
+        }
+        prosjek << "Prosjecna ocjena iz predmeta programiranja za navedene ucenike: " << suma / brojac;
+    }
     return 0;
 }
 /*
