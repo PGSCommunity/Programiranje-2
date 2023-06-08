6ff901610aeb643ffb85f45da462c1369fd94366
Danin Sadzak <daninsadzak@gmail.com>
2023-06-08 20:29:53 +0200

    Testovi - Datoteke - Postavljen 1. zadatak

---
+++ Testovi/Datoteke/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji iz postojece datoteke cita realne brojeve i ispisuje ih na ekran pomnozene sa x.
+*/
