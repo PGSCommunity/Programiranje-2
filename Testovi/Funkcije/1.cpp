1a85d7208757500e3ba3f01acaebce41f51fedfd
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:41:03 +0200

    Testovi - Funkcije - Postavljen 1. zadatak

---
+++ Testovi/Funkcije/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program u kojem se nakon ucitavanja cijelog broja pozivaju dvije funkcije:
+    1. BrojIme ispisuje dvostruku vrijednost ucitanog broja ukoliko je broj veci od 10
+    2. BrojPrezime vraca kvadratni korijen broja ako je broj manji od 50.
+*/
