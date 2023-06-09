8eabf6637f53f4e0d0150033b8b3846b1d907b7d
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:52:55 +0200

    Testovi - Funkcije - Zavrsen 2. zadatak

--- Testovi/Funkcije/2.cpp
+++ Testovi/Funkcije/2.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int brojBrojeva(int n, int m) {
+    int brojac(0);
+    for(int i = n; i <= m; i++) {
+        if(i % 10 == m) brojac++;
+    }
+    return brojac;
+}
 
+int proizvodBrojeva(int n, int m) {
+    int proizvod(1);
+    for(int i = n; i <= m; i++) {
+        if(i % m != 0) proizvod *= i;
+    }
+    return proizvod;
+}
+
+int main() {
+    int n, m;
+    cout << "Unesite brojeve n i m!\n";
+    cin >> n >> m;
+
+    cout << "Broj brojeva cija je cifra jedinice jednaka m: " << brojBrojeva(n, m) << '\n';
+    cout << "Proizvod brojeva koji nisu djeljivi sa m: " << proizvodBrojeva(n, m);
     return 0;
 }
 /*
