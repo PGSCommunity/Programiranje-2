ec51414d5059ec9842b7562021afdfd74dec5637
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:17:59 +0200

    Testovi - Ponavljanje - Postavljen 1. zadatak

---
+++ Testovi/Ponavljanje/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji omogucava unos dvocifrenog broja.
+Ukoliko je broj manji ili jednak 50 racuna se i ispisuje suma njegovih cifara. Za broj veci od 50 racuna se i ispisuje kvadratni
+korijen tog broja. Ukoliko broj nije dvocifren, ispisuje se adekvatna poruka i omogucava ponovni unos broja.
+*/
