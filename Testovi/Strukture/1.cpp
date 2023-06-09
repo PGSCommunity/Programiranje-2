2723e60660dc873ed79a43f64626da15ec476aab
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 23:14:00 +0200

    Testovi - Strukture - Postavljen 1. zadatak

---
+++ Testovi/Strukture/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu valjak (r i h). Zatim napisati program koji sadrzi dvije funkcije:
+    1. Funkcija koja omogucava unos podataka definiranim strukturom
+    2. Funkcija koja raca vrijednost povrsine valjka (P = 2rPI(r + h)).
+Kao rezultat se ispisuje reciprocna vrijednost povrsine valjka.
+*/
