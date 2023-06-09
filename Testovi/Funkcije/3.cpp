410d9ffc14a734eef81ff0e633dbcedd5fc385bc
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:56:52 +0200

    Testovi - Funkcije - Postavljen 3. zadatak

---
+++ Testovi/Funkcije/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati funkcije koje za uneseni cijeli broj ispituje i ispisuje poruku o tome da li je taj broj:
+    I. Savrsen 
+    II. Prost
+    III. Armstrongov
+*/
