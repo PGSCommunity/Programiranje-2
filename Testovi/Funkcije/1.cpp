4bd5e73e18b498a4841be0f980c420077fdc0fe3
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:44:18 +0200

    Testovi - Funkcije - Zavrsen 1. zadatak

--- Testovi/Funkcije/1.cpp
+++ Testovi/Funkcije/1.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
+void brojIme(int x) {
+    if(x > 10) {
+        cout << "Dvostruka vrijednost ucitanog broja: " << x * 2 << '\n';
+    } else {
+        cout << "Broj je manji od 10!\n";
+    }
+}
 
+float brojPrezime(int x) {
+    if(x < 50) {
+        return sqrt(x);
+    }
+}
+
+int main() {
+    int x;
+    cout << "Unesite broj!\n";
+    cin >> x;
+
+    brojIme(x);
+    cout << brojPrezime(x);
     return 0;
 }
 /*
