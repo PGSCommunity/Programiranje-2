9aeda7502dd664dc829c62e3eeadae62195287a4
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:47:00 +0200

    Testovi - Funkcije - Postavljen 2. zadatak

---
+++ Testovi/Funkcije/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program u kojem se nakon ucitavanja broja n i m (u main funkciji) pozivaju funkcije koje racunaju i vracaju sljedece
+vrijednosti:
+    -   Broj brojeva cija je cifra jedinice jednaka m
+    -   Proizvod brojeva koji nisu djeljivi sa m
+*/
