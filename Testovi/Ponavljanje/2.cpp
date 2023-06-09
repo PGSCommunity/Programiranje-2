d82552d47dd95a5dc41f247991957baf52c97206
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:28:47 +0200

    Testovi - Ponavljanje - Zavrsen 2. zadatak

--- Testovi/Ponavljanje/2.cpp
+++ Testovi/Ponavljanje/2.cpp
@@ -2,7 +2,23 @@
 using namespace std;
 
 int main() {
+    size_t suma(0), proizvod(1);
 
+    int i(100);
+
+    while(i <= 999) {
+        if(i % 2 == 0) {
+            suma += i;
+            proizvod *= i;
+        }
+        i++;
+    }
+
+    cout << "Suma parnih trocifrenih brojeva: " << suma << '\n';
+    cout << "Proizvod parnih trocifrenih brojeva: " << proizvod;
+
+    //Ovaj program ne moze izracunati proizvod, zato sto je broj previse visok.
+    //Nema veze i ako koristimo size_t, ili long long int, uvijek ce ispisat da je proizvod 0 zbog previsokog broja.
     return 0;
 }
 /*
