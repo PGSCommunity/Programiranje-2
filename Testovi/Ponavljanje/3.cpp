c190c620b10863a84be52ee363bff65c0f3b772a
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:30:26 +0200

    Testovi - Ponavljanje - Postavljen 3. zadatak

---
+++ Testovi/Ponavljanje/3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji omogucava deklaraciju i unos clanova niza dimenzije n (n >= 5). Potom se ispisuje prvi, treci i posljedni
+clan niza, te aritmeticka sredina clanova niza.
+*/
