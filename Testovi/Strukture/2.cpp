d0de81d1e7edc88b92e030e6c8a837956f32ddf1
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 23:33:34 +0200

    Testovi - Strukture - Zavrsen 2. zadatak

--- Testovi/Strukture/2.cpp
+++ Testovi/Strukture/2.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct slika {
+    string ime;
+    string autor;
+    float cijena;
+    int dimenzija;
+};
 
+int main() {
+    int m;
+    cout << "Koliko slika?\n";
+    cin >> m;
+    slika s[m];
+
+    for(int i = 0; i < m; i++) {
+        cout << "Unesite ime " << i + 1 << ". slike!\n";
+        cin >> s[i].ime;
+        cout << "Unesite autora " << i + 1 << ". slike!\n";
+        cin >> s[i].autor;
+        cout << "Unesite cijenu " << i + 1 << ". slike!\n";
+        cin >> s[i].cijena;
+        cout << "Unesite dimenziju " << i + 1 << ". slike!\n";
+        cin >> s[i].dimenzija;
+        cout << "====================\n\n"; 
+    }
+
+    int maximum(0), minimum(m);
+    int index1(0), index2(0);
+
+    for(int i = 0; i < m; i++) {
+        if(maximum < s[i].cijena) {
+            maximum = s[i].cijena;
+            index1 = i;
+        }
+    }
+    
+    cout << "PODACI O NAJSKUPLJOJ SLICI:\n";
+    cout << "Ime: " << s[index1].ime << '\n';
+    cout << "Ime autora: " << s[index1].autor << '\n';
+    cout << "Cijena slike: " << s[index1].cijena << '\n';
+    cout << "Dimenzija slike " << s[index1].dimenzija << "\n\n";
+    
+    for(int i = 0; i < m; i++) {
+        if(minimum > s[i].dimenzija) {
+            minimum = s[i].dimenzija;
+            index2 = i;
+        }
+    }
+
+    cout << "NAZIV SLIKE NAJMANJE DIMENZIJE:\n";
+    cout << s[index2].ime << "\n\n";
+
+    cout << "IMENA AUTORA CIJA JE CIJENA SLIKE VECA OD 1,000$:\n";
+    for(int i = 0; i < m; i++) {
+        if(s[i].cijena > 1000) {
+            cout << s[i].autor << '\n';
+        }
+    }
     return 0;
 }
 /*
