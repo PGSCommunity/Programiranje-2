303a0664ea6977f052599a56cb2848029a00686a
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:22:15 +0200

    Testovi - Ponavljanje - Zavrsen 1. zadatak

--- Testovi/Ponavljanje/1.cpp
+++ Testovi/Ponavljanje/1.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
+    float x;
+    cout << "Unesite dvocifreni broj!\n";
+    unos:cin >> x;
 
+    if(!(x >= 10 && x <= 99)) {
+        cout << "Broj nije dvocifren! Pokusaj te ponovo!\n";
+        goto unos;
+    }
+
+    if(x <= 50) {
+        int suma(0);
+        for(int i = 1; i <= x; i++) {
+            suma += i;
+        }
+        cout << "Suma cifara: " << suma;
+    } else {
+        cout << "Kvadratni korijen broja: " << sqrt(x);
+    }
     return 0;
 }
 /*
