645d82bcc030f9b097cc3df2dbb9a0a388fa94b1
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 22:34:02 +0200

    Testovi - Ponavljanje - Zavrsen 3. zadatak

--- Testovi/Ponavljanje/3.cpp
+++ Testovi/Ponavljanje/3.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 
 int main() {
+    int n;
+    cout << "Velicina niza?\n";
+    unos:cin >> n;
 
+    if(!(n >= 5)) {
+        cout << "Velicina niza mora biti veca ili jednaka broju 5! Pokusaj te ponovo:\n";
+        goto unos;
+    }
+
+    int niz[n];
+    float suma(0);
+    for(int i = 0; i < n; i++) {
+        cout << "Unesite " << i + 1 << ". clan niza!\n";
+        cin >> niz[i];
+        suma += niz[i];
+    }
+
+    cout << "Prvi clan niza: " << niz[0] << '\n';
+    cout << "Treci clan niza: " << niz[2] << '\n';
+    cout << "Posljedni clan niza: " << niz[n - 1] << '\n';
+    cout << "Aritmeticka sredina clanova niza: " << suma / n;
     return 0;
 }
 /*
