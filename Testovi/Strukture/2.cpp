5ac1a02c55bbc36ce737e7b695a5a0e008e64a53
Danin Sadzak <daninsadzak@gmail.com>
2023-06-09 23:21:50 +0200

    Testovi - Strukture - Postavljen 2. zadatak

---
+++ Testovi/Strukture/2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Definirati strukturu slika (naziv slike, ime autora, cijena, dimenzija). Zatm za m unesenih slika ispisati:
+    - Sve podatke o najskupljoj slici
+    - Naziv slike najmanje dimenzije
+    - Imena autora slika cija je cijena veca od 1,000$
+*/
