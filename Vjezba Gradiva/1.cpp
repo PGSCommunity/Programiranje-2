34e6bdcdb796e66c8efb35b599def16b5affd1f1
Danin Sadzak <daninsadzak@gmail.com>
2023-06-10 22:24:43 +0200

    Vjezba Gradiva - Postavljen 1. zadatak

---
+++ Vjezba Gradiva/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Napisati program koji od korisnika traži unos prirodnog  broja 𝑛. Program ispisujeprvih 𝑛 potpunih kvadrata koji nisu parni 
+brojevi. Ukoliko korisnik unese prirodan broj 2, program ispisuje 1 i 9. Broj je potpun kvadrat ukoliko se on može napisati kao 
+kvadrat prirodnog broja.
+*/
