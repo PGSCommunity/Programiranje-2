14c1b1c39a1db8839f92c5717fa31d75f9078428
Danin Sadzak <daninsadzak@gmail.com>
2023-06-10 22:03:55 +0200

    Testovi - Typedef - Postavljen 1. zadatak

---
+++ Testovi/Typedef/1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    return 0;
+}
+/*
+Ucitati cijele brojeve w i z, te odrediti vrijednost sljedeceg izraza: 
+                    9sqrt(z)
+    za w > 2 =-------------------
+              |sin(z) - 7sqrt(w)| 
+
+               sqrt(w + z)
+    za z < -2 =-----------
+                 |w - z|
+
+Varijable w i z su cjelobrojne korisnicki definirane (typedef) a x je realna varijabla.
+*/
